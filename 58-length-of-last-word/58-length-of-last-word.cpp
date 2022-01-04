8bc0c4a04389f34c21edd20b438d7626fd2471d2
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-01-04 15:37:41 +0530

    Time: 4 ms (58.32%), Space: 7.1 MB (5.93%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.cpp
+++ 58-length-of-last-word/58-length-of-last-word.cpp
@@ -1,5 +1,12 @@
 class Solution {
 public:
+    #define db1(x) 						  cout<<#x<<"= "<<x<<'\n'
+#define db2(x,y) 					  cout<<#x<<"= "<<x<<", "<<#y<<"= "<<y<<'\n'
+#define db3(x,y,z) 				  cout<<#x<<"= "<<x<<", "<<#y<<"= "<<y<<", "<<#z<<"= "<<z<<'\n'
+    
+    
+    
+    
     int lengthOfLastWord(string s) {
         // int n = s.length() ; 
         // int i = n - 1 ;
@@ -15,12 +22,40 @@ public:
         
         
         
-        int len = 0, tail = s.length() - 1;
-        while (tail >= 0 && s[tail] == ' ') tail--;
-        while (tail >= 0 && s[tail] != ' ') {
-            len++;
-            tail--;
-        }
-        return len;
+//         int len = 0, tail = s.length() - 1;
+//         while (tail >= 0 && s[tail] == ' ') tail--;
+//         while (tail >= 0 && s[tail] != ' ') {
+//             len++;
+//             tail--;
+//         }
+//         return len;
+        
+        
+        
+        
+         stringstream ss;
+        ss<<s;
+       
+        string temp;
+        while(ss>>temp){ }
+        return temp.size();
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 };
