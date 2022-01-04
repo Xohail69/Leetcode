9f8a66cac87562c97a522a136730186a2d2515ea
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-01-04 15:14:34 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (18.20%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    string mergeAlternately(string w1, string w2) {
+       int n = w1.length() , m = w2.length() ;
+        string s ;
+     
+        ///Using Two Prs 
+        int i(0) , j(0)  ;
+        while ( i < n or j < m ) {
+            if(i < n ) s += w1[i++] ;
+            if(j < m ) s += w2[j++] ;
+        }
+        
+        return s ;
+        
+        
+        //Using One Pr
+       
+//         for(int i = 0 ; i < max(n , m ) ; i++ ) {
+//             if(i < n ) s += w1[i] ;
+//             if(i < m ) s += w2[i] ;
+            
+//         }
+//         return s ;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+};
