10005667a4e62c69a87d3066fb77240be378c57c
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-01-05 18:40:15 +0530

    Time: 5 ms (11.98%), Space: 7.1 MB (61.31%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,15 +1,7 @@
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        
-        vector<int> sub ;
-        vector<vector<int>> ans ;
-        dfs(ans , sub , nums , 0 ) ;
-        return ans ;
-        
-    }
     
-   void dfs(vector<vector<int>> &ans , vector<int> &sub , vector<int> &nums , int start){
+       void dfs(vector<vector<int>> &ans , vector<int> &sub , vector<int> &nums , int start){
     ans.push_back(sub) ;
      for(int i = start ; i < nums.size() ; i++ ) {
                 sub.push_back(nums[i]) ;
@@ -19,4 +11,51 @@ public:
         }
     
    }
+    
+    
+    
+    vector<vector<int>> subsets(vector<int>& nums) {
+        //M1 using Backtracking
+        // vector<int> sub ;
+        // vector<vector<int>> ans ;
+        // dfs(ans , sub , nums , 0 ) ;
+        // return ans ;
+        
+        
+        //More simple & cool solution 
+        //https://leetcode.com/problems/subsets/discuss/27511/C%2B%2B-8ms-simple-iterative-solution
+        vector<vector<int>> ans(1 , vector<int>() ) ;
+        sort(begin(nums) , end(nums) ) ;
+        for(int i = 0 ; i < nums.size() ; i++ ) {
+            int n = ans.size() ;
+            for(int j = 0 ; j < n  ; j++  ) {
+                // ans.push_back(ans[j]) ; //push back the jth subvector of ans into ans 
+                // ans.back().push_back(nums[i]) ; //push back nums et to the subvector pushed above
+                
+                //Or you can think like this 
+                vector<int> t = ans[j] ;
+                t.push_back(nums[i]) ;
+                ans.push_back(t) ;   
+            }
+        }
+        return ans ; 
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+  
+
+    
+    
+    
+    
+    
+    
+    
 };
