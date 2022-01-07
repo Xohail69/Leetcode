1d64c02cf87d6d54f4aecedba5eaa1b8ab863b1e
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-01-07 21:12:38 +0530

    Time: 28 ms (27.46%), Space: 16.7 MB (48.28%) - LeetHub

--- 382-linked-list-random-node/382-linked-list-random-node.cpp
+++ 382-linked-list-random-node/382-linked-list-random-node.cpp
@@ -12,19 +12,45 @@ class Solution {
 public:
     int l = 0 ; 
     ListNode* HEAD ;
+     vector<int> v ;
+    
+    
     Solution(ListNode* head) {
-        ListNode* p = head ;
-        HEAD = head ;
-        while(head) {
-            l++ ; head = head->next ;
-        }
+       
+        // HEAD = head ;
+        // while(head) {
+        //     l++ ; head = head->next ;
+        // }
+        
+        
+       
+         ListNode* p = head ;
+        for( ; p ; p = p->next ) v.push_back(p->val) ;
+        
     }
     
     int getRandom() {
-        int idx = rand()%l ;
-        ListNode* t = HEAD ;
-        for(int i = 0 ; i < idx ; i++ ) { t= t->next ; }
-        return t->val ;
+        // int idx = rand()%l ;
+        // ListNode* t = HEAD ;
+        // for(int i = 0 ; i < idx ; i++ ) { t= t->next ; }
+        // return t->val ;
+        
+        
+        
+        int idx = rand()%v.size() ;
+        return v[idx] ;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 };
 
