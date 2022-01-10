d8804c86e7c5f24f2ea46e0044fed53787902ad2
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-01-10 22:22:27 +0530

    Time: 7 ms (13.33%), Space: 6.1 MB (96.62%) - LeetHub

--- 67-add-binary/67-add-binary.cpp
+++ 67-add-binary/67-add-binary.cpp
@@ -6,15 +6,15 @@ public:
         while( i >= 0 or j >= 0 or c == 1 ){
             c += i>=0 ? a[i--] - '0' : 0 ;
             c += j>=0 ? b[j--] - '0' : 0 ;
-            s =  char(c % 2 + '0') + s  ; //c%2 means insert 0 if carry==0  or carry==2 (1+1) because ('1'+'1'=0 in binary) else insert 1 ('0'+'1'=1). Adding '0' to convert in ascii from integer 
+            //s +=  char(c % 2 + '0')   ; //c%2 means insert 0 if carry==0  or carry==2 (1+1) because ('1'+'1'=0 in binary) else insert 1 ('0'+'1'=1). Adding '0' to convert in ascii from integer 
             //We can also write above line as 
-            //s += char( (c & 1) + '0') ;
+            s += char( (c & 1) + '0') ;
             
             
             c /= 2; //if c==2 or 3, there is a carry i.e 1 otherwise no carry (3 in case if prev c=1 and a[i]=1 and b[i]=1)
         
         }
-        //reverse(begin(s) , end(s) )  ;
+        reverse(begin(s) , end(s) )  ;
         return s ;
      
         
