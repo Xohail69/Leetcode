475dbc21bac1aa83a9597dcde698cd8dcc3a884a
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-01-12 19:18:29 +0530

    Time: 84 ms (82.13%), Space: 57 MB (49.59%) - LeetHub

--- 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.cpp
+++ 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.cpp
@@ -12,13 +12,53 @@
 class Solution {
 public:
     TreeNode* insertIntoBST(TreeNode* root, int val) {
-        
+        //recursive
         if(!root){
-            TreeNode *t = new TreeNode(val) ;
-            return t ;
+     
+            return new TreeNode(val) ;
         }
         if(val < root->val ) root->left = insertIntoBST(root->left , val) ;
-        else  if(val > root->val ) root->right = insertIntoBST(root->right , val) ;
+        else   root->right = insertIntoBST(root->right , val) ;
         return root ;
+        
+        
+        //Iterative
+//          if(!root) return new TreeNode(val);
+        
+//         auto curr = root;
+        
+//         while(true){
+//             if(curr->val < val){
+//                 if(curr->right) curr = curr->right;
+//                 else {
+//                     curr->right = new TreeNode(val);
+//                     break;
+//                 }
+//             }
+//             else{
+//                 if(curr->left) curr = curr->left;
+//                 else{
+//                     curr->left = new TreeNode(val);
+//                     break;
+//                 }
+//             }
+//         }
+//         return root;
+        
+        
+        
+        
+        
+        
+        //same iterative but more compact 
+        
+		// TreeNode **cur = &root;
+		// while( *cur )
+		// 	cur = (val > (*cur)->val) ? &(*cur)->right : &(*cur)->left;
+		// *cur = new TreeNode(val);
+		// return root;
+        
+        
+        
     }
 };
