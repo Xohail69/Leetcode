40d824c742ba880f51503212049a8b081297454f
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-01-19 15:49:31 +0530

    Time: 11 ms (40.57%), Space: 7.4 MB (89.55%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
@@ -9,19 +9,35 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        if( !head or !head->next ) return NULL ;
-        vector<ListNode*>v;
+//         if( !head or !head->next ) return NULL ;
+//         vector<ListNode*>v;
         
-        while(head){
-            auto it = find(v.begin(), v.end(), head) ;
-            if(it != v.end())
-                return *it;
-            v.push_back(head);
-            head = head->next;
+//         while(head){
+//             auto it = find(v.begin(), v.end(), head) ;
+//             if(it != v.end())
+//                 return *it;
+//             v.push_back(head);
+//             head = head->next;
+//         }
+//         return NULL ;
+        
+        //Using floyd cycle detection 
+         ListNode* slow = head;
+        ListNode* fast = head;
+        
+        while(fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast){
+                slow = head;
+                while(slow != fast){
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                return slow;
+            }
         }
-        return NULL ;
-        
-        
+        return NULL;
         
     
         
