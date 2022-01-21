0bb4c84b30c3a5a39d6c1efc031a54c623345728
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-01-21 22:02:38 +0530

    Time: 60 ms (53.56%), Space: 49.7 MB (98.95%) - LeetHub

--- 134-gas-station/134-gas-station.cpp
+++ 134-gas-station/134-gas-station.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     int canCompleteCircuit(vector<int>& g, vector<int>& c) {
         
+//         why we're not going back to the middle element ( from where we've started our journey) from the end to just check whether it's possible to make circular tour or not? It's because we've already checked previously that it's possible to come from the 0'th index to the middle. So no need to check for the remainig part of the circular tour since it will always be a valid tour.
+        
         int start(0) , current(0) ;
         if( accumulate(begin(g) , end(g) , 0) < accumulate(begin(c) , end(c) , 0) ) 
             return -1 ;
