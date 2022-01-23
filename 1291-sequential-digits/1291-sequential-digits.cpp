ed25b29d75c6539c8ddfb0518d9bc49526336fe2
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-01-23 22:23:47 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (5.00%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.cpp
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -11,32 +11,32 @@ public:
         //M1 Create all possible numbers & if number lies b/w l & h then add
         // it to the ans 
         // TC - O(n) or say constant 
-        vector<int>all = {  12,23,34,45,56,67,78,89,
-                         123,234,345,456,567,678,789,
-                         1234,2345,3456,4567,5678,6789,
-                         12345,23456,34567,45678,56789,
-                         123456,234567,345678,456789,
-                         1234567,2345678,3456789,
-                         12345678,23456789,
-                         123456789
-        };
-        vector<int> ans ;
-        for(int i = 0 ; i < all.size() ; i++ )  {
-             if(all[i] < low ) continue ;
-             if(all[i] > high) break ;
-            ans.push_back(all[i]) ;
-        }
-        return ans ;
+        // vector<int>all = {  12,23,34,45,56,67,78,89,
+        //                  123,234,345,456,567,678,789,
+        //                  1234,2345,3456,4567,5678,6789,
+        //                  12345,23456,34567,45678,56789,
+        //                  123456,234567,345678,456789,
+        //                  1234567,2345678,3456789,
+        //                  12345678,23456789,
+        //                  123456789
+        // };
+        // vector<int> ans ;
+        // for(int i = 0 ; i < all.size() ; i++ )  {
+        //      if(all[i] < low ) continue ;
+        //      if(all[i] > high) break ;
+        //     ans.push_back(all[i]) ;
+        // }
+        // return ans ;
         
         
         
         //M2 Using Rec keep generating the number till it lies in the given range 
         //O(nlogn)
         //https://leetcode.com/problems/sequential-digits/discuss/1711942/C%2B%2B-DFS-with-diagram-or-Basic-0-ms
-        // res = {} ;
-        // for(int i = 1 ; i <= 9  ; i++ ) dfs(low , high , i , 0) ;
-        //     sort(res.begin() , end(res)) ;
-        // return res ;
+        res = {} ;
+        for(int i = 1 ; i <= 9  ; i++ ) dfs(low , high , i , 0) ;
+            sort(res.begin() , end(res)) ;
+        return res ;
         
         //this recursive M2 can be replaced by iteration as folllows :
 //          vector<int> ans;
