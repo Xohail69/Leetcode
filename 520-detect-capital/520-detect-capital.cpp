ce4216ce143546790324817f5fca37212ac76879
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-01-24 20:28:05 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (67.65%) - LeetHub

--- 520-detect-capital/520-detect-capital.cpp
+++ 520-detect-capital/520-detect-capital.cpp
@@ -5,17 +5,61 @@ public:
     }
     
     bool detectCapitalUse(string word) {
-        int c = 0  ; 
-        for (auto x : word ) {
-            if( iscap(x) ) c++  ;
-        }
-        if(c == 1){
-            if(iscap(word[0]) ) return 1 ;
-            else return 0 ; 
-        }
+//         int c = 0  ; 
+//         for (auto x : word ) {
+//             if( iscap(x) ) c++  ;
+//         }
+//         if(c == 1){
+//             if(iscap(word[0]) ) return 1 ;
+//             else return 0 ; 
+//         }
         
-        if(c > 1 and c < word.size() ) return 0 ;
-        else return 1 ;
+//         if(c > 1 and c < word.size() ) return 0 ;
+//         else return 1 ;
+        
+        
+         bool firstCaps = isupper(word[0]), allCaps = true, allSmall = true;
+        for(int i=1;i<word.size();i++){
+            if(isupper(word[i])){
+                allSmall = false;
+            }else{
+                allCaps = false;
+            }
+        }
+        return ( firstCaps && allCaps) || allSmall ;
+        
+        
+        
+        
+        
+        
+        
+        //  for(int i = 1; i < word.size(); i++){
+        // if(isupper(word[1]) != isupper(word[i]) || 
+        //    islower(word[0]) && isupper(word[i])) return false;
+        //     }
+        // return true ;
+//         if(isupper(word[1]) != isupper(word[i])) return false;
+// This code checks if all are upper case AND check if all are lower case.
+// You can also replace it by if(islower(word[1]) != islower(word[i])) return false;
+
+// if( islower(word[0]) && isupper(word[i])) return false;
+// Counter example of "Only the first letter in this word is capital, like "Google""
+//     return true;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
        
     }
 };
