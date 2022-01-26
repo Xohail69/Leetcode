9b321807b19e279b3d209efcba0d32ecc68da8da
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-01-26 22:16:35 +0530

    Time: 249 ms (23.91%), Space: 84.5 MB (79.25%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -27,20 +27,20 @@ public:
         // return v ;
         
         //M2 using Merging
-        vector<int>a , b ;
-         inorder(root1 , a ) ;
-        inorder(root2 , b ) ;
-        vector<int> ans(a.size()+b.size());
-        int i=0,j=0,k=0;
-        while(i<a.size() and j<b.size())
-            ans[k++]=a[i]<b[j] ? a[i++] : b[j++];
+//         vector<int>a , b ;
+//          inorder(root1 , a ) ;
+//         inorder(root2 , b ) ;
+//         vector<int> ans(a.size()+b.size());
+//         int i=0,j=0,k=0;
+//         while(i<a.size() and j<b.size())
+//             ans[k++]=a[i]<b[j] ? a[i++] : b[j++];
 
-        while(i<a.size())
-            ans[k++]=a[i++];
+//         while(i<a.size())
+//             ans[k++]=a[i++];
 
-        while(j<b.size())
-            ans[k++]=b[j++];
-        return ans;
+//         while(j<b.size())
+//             ans[k++]=b[j++];
+//         return ans;
         
         
         
@@ -49,37 +49,39 @@ public:
 // Create stack for both tree and push element until root->left is NULL.
 // Compare the top of both stack and push the smaller element in the result array.
 // if any of the stack is empty we will push the top element of other stack and change root = root->rightand same condition will be taken care for other stack.
-// //         vector<int> res;
-//           stack<TreeNode *> s1, s2;
-
-//           while (root1 || root2 || !s1.empty() || !s2.empty())
-//           {
-//                while (root1 != NULL)
-//                {
-//                     s1.push(root1);
-//                     root1 = root1->left;
-//                }
-//                while (root2 != NULL)
-//                {
-//                     s2.push(root2);
-//                     root2 = root2->left;
-//                }
-//                if (s2.empty() || (!s1.empty() && s1.top()->val <= s2.top()->val))
-//                {
-//                     root1 = s1.top();
-//                     s1.pop();
-//                     res.push_back(root1->val);
-//                     root1 = root1->right;
-//                }
-//                else
-//                {
-//                     root2 = s2.top();
-//                     s2.pop();
-//                     res.push_back(root2->val);
-//                     root2 = root2->right;
-//                }
-//           }
-//           return res;
+        
+        
+        
+           vector<int> res;
+          stack<TreeNode *> s1, s2;
+          while (root1 || root2 || !s1.empty() || !s2.empty())
+          {
+               while (root1 != NULL)
+               {
+                    s1.push(root1);
+                    root1 = root1->left;
+               }
+               while (root2 != NULL)
+               {
+                    s2.push(root2);
+                    root2 = root2->left;
+               }
+               if (s2.empty() || (!s1.empty() && s1.top()->val <= s2.top()->val))
+               {
+                    root1 = s1.top();
+                    s1.pop();
+                    res.push_back(root1->val);
+                    root1 = root1->right;
+               }
+               else
+               {
+                    root2 = s2.top();
+                    s2.pop();
+                    res.push_back(root2->val);
+                    root2 = root2->right;
+               }
+          }
+          return res;
         
         
         
