1da00c98549920bde95346e9182308635bfc6252
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-01-26 22:11:38 +0530

    Time: 176 ms (47.82%), Space: 85.3 MB (48.31%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -11,18 +11,38 @@
  */
 class Solution {
 public:
-    vector<int>v ;
-    void inorder(TreeNode *root) {
+   
+    void inorder(TreeNode *root , vector<int> &v) {
         if( !root ) return ;
-        inorder(root->left) ;
-        v.push_back(root->val) ;
-        inorder(root->right) ;
+        inorder(root->left , v) ;
+        v.push_back(root->val) ; 
+        inorder(root->right , v) ;
     }
     vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
-        v = {} ;
-        inorder(root1) ;
-        inorder(root2) ;
-        sort(v.begin() , v.end() ) ;
-        return v ;
+        // v = {} ;
+        // inorder(root1) ;
+        // inorder(root2) ;
+        // sort(v.begin() , v.end() ) ;
+        // return v ;
+        
+        //M2
+        vector<int>a , b ;
+         inorder(root1 , a ) ;
+        inorder(root2 , b ) ;
+        vector<int> ans(a.size()+b.size());
+        int i=0,j=0,k=0;
+        while(i<a.size() and j<b.size())
+            ans[k++]=a[i]<b[j] ? a[i++] : b[j++];
+
+        while(i<a.size())
+            ans[k++]=a[i++];
+
+        while(j<b.size())
+            ans[k++]=b[j++];
+
+        return ans;
+        
+        
+        
     }
 };
