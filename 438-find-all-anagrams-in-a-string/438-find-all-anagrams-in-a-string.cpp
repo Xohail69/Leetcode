1e411c62a62aa074fadb1ef705e0e991199a95bd
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-02-02 18:43:42 +0530

    Time: 26 ms (44.23%), Space: 8.7 MB (76.26%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -5,14 +5,16 @@ public:
         if(m < n) return {} ;
         vector<int>ans ; 
         vector<int> ph(26 , 0 ) , sh(26 , 0 ) ;
+        //First window filled in the HT
         for(int i = 0 ; i < n ; i++ ){
             ph[p[i] - 'a']++ ;
             sh[s[i] - 'a']++ ;
         }
         if( ph == sh ) ans.push_back(0 ) ;
+        //Sliding the windows , moving L to R
         for(int i = n ; i < m ; i++ ){
-            sh[s[i] - 'a']++ ;
-            sh[s[i - n] - 'a' ]-- ;
+            sh[s[i] - 'a']++ ; // sliding right side to 1 by Adding the new char in the hash from the right
+            sh[s[i - n] - 'a' ]-- ; // sliding left side to 1 by Removing the char
             if( ph == sh ) ans.push_back(i - n + 1 ) ;
         }
         
