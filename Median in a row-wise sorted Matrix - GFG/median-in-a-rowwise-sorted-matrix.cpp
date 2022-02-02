216c2555f66b9672e97c99325066a47b8ce08cbe
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-02-02 23:37:11 +0530

    Added solution - LeetHub

--- Median in a row-wise sorted Matrix - GFG/median-in-a-rowwise-sorted-matrix.cpp
+++ Median in a row-wise sorted Matrix - GFG/median-in-a-rowwise-sorted-matrix.cpp
@@ -10,7 +10,12 @@ using namespace std;
 
 class Solution{   
 public:
+//Instead of using this fuction we can directly use upperbound 
     int countSmallerEqual(vector<int> &a , int target ) {
+        //We are finding a number which is just greater than the target becuz 
+        //its index can give us the count of numbers which are smaller or equal to 
+        // target .
+        
         int l(0) , h(a.size() - 1) , mid ;
         while(l <= h ) {
             mid = l + (h-l)/2 ; 
@@ -20,16 +25,22 @@ public:
         return l ;
     }
     int median(vector<vector<int>> &A , int r, int c){
-        // code here    
-        
+        //If n is total number of elements & is odd then 
+        // The idea is that for a number to be median there should be exactly (n/2) numbers 
+        //which are less than this number. So, we try to find the count of numbers less than
+        //all the numbers.   
+        // here our n is r*c ;
         int l = 1  , h = 2001 , mid ;
         while(l <= h ){
-            mid = l + (h-l)/2 ;
+            mid = l + (h-l)/2 ; //Choosing a potential candidate for median 
             int cunt = 0 ;
             for(int i = 0 ; i < r ; i ++ ) {
-                cunt += countSmallerEqual(A[i] , mid) ;
+                //counting row wise how many elements are smaller or equal to mid in 
+                // and adding them 
+                //cunt += countSmallerEqual(A[i] , mid) ;
+                cunt += upper_bound(A[i].begin() ,A[i].end() , mid) - A[i].begin() ;
             }
-            if(cunt <= (r*c)/2 ) l = mid + 1 ;
+            if(cunt <= (r*c)/2 ) l = mid + 1 ; //if count is less than n/2 then increase l
             else h = mid - 1 ; 
         }
         return l ;
