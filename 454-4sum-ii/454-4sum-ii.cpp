cf0a87dd20db3acd9a05e1f7962c2449121f80c1
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-02-03 21:51:34 +0530

    Time: 176 ms (96.35%), Space: 24.4 MB (49.44%) - LeetHub

--- 454-4sum-ii/454-4sum-ii.cpp
+++ 454-4sum-ii/454-4sum-ii.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+//         Count number of solutions to a + b + c + d  = 0
+// ⇒ a + b = -(c + d)
+// Iterate through the first 2 arrays and count the frequency of all possible sums of pairs
+// Iterate through the other 2 arrays and sum up the frequency of -(c + d) where c and d are all possible pairs in these 2 arrays. -(c + d) should be equal to a + b for a valid solution.
+// The number of such matches is the required result
+
         unordered_map<int,int> m ;
         for(auto i : nums1) {
             for(auto j : nums2 ) m[i+j]++ ;
