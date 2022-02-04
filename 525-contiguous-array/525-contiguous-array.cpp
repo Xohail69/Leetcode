e5adf2d28b8d0ea3835a337d6a0894cb6f5e2ef4
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-02-04 21:10:17 +0530

    Time: 217 ms (30.41%), Space: 83.7 MB (97.48%) - LeetHub

--- 525-contiguous-array/525-contiguous-array.cpp
+++ 525-contiguous-array/525-contiguous-array.cpp
@@ -22,7 +22,18 @@ public:
         
         
         //M2 
+//    
+        
+        
+//         We use a variable sum, which is used to store the relative number of ones and zeros encountered so far.
+// sum is incremented by one whenever a 1 is encountered and decremented by one whenever a 0 is encountered.
+// We traverse the array nums from the beginning.
+// If at any index, sum becomes zero, it means that we've encountered equal number of zeros and ones from the beginning till the current index.
+// And at any index, we encounter a sum that's already been encountered at some previous index, it means that the number of 0's and 1's are equal between the indices corresponding to the equal sum values.
+// We keep track of the indices that correspond to the same sum with maximum difference between the indices to find the maximum length of the subarrays with equal number of 0's & 1's.
+        
         unordered_map<int , int> m ;
+        //We'll map prefix sum with the index
         int ans =  0 ;
         int sum = 0 ;
       
@@ -30,7 +41,9 @@ public:
             
             sum += (nums[i]==0) ? -1 : 1 ;
             
-            if(sum == 0 ) ans = max(ans , i + 1 ) ;
+             // If sum becomes 0, it means array from the beginning contains equal number of 0's and 1's.
+            if(sum == 0 ) ans = max(ans , i + 1 ) ; 
+            
             
             if(m.count(sum) ) ans = max(ans , i - m[sum]) ;
             else m[sum] = i ;
