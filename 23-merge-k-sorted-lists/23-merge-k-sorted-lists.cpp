9c7ecac15c9fa53af5b5314daf4302ee14d1d5a4
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-02-05 18:50:45 +0530

    Time: 30 ms (60.27%), Space: 13.2 MB (74.95%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -31,58 +31,58 @@ public:
     
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         //The basic idea is really simple. We can merge first two lists and then push it back. Keep doing this until there is only one list left in vector. Actually, we can regard this as an iterative divide-and-conquer solution.
-        if(lists.empty()) return NULL ; 
+//         if(lists.empty()) return NULL ; 
         
-        while(lists.size() > 1 ) {
-            lists.push_back(mergeTwo(lists[0] , lists[1])) ;
-            lists.erase(lists.begin()) ;
-            lists.erase(lists.begin()) ;
-        }
-        return lists[0] ;
+//         while(lists.size() > 1 ) {
+//             lists.push_back(mergeTwo(lists[0] , lists[1])) ;
+//             lists.erase(lists.begin()) ;
+//             lists.erase(lists.begin()) ;
+//         }
+//         return lists[0] ;
         
         
         
         
         
         //M2 using PQ 
-//         if(lists.empty()) return NULL ; 
-//         auto comp = [&](ListNode *a , ListNode *b) {
-//             return a->val > b->val ;
-//         };
-//         // This priority queue is our MIN HEAP
-//         priority_queue<ListNode* , vector<ListNode*> , decltype(comp)> pq(comp) ;
+        if(lists.empty()) return NULL ; 
+        auto comp = [&](ListNode *a , ListNode *b) {
+            return a->val > b->val ;
+        };
+        // This priority queue is our MIN HEAP
+        priority_queue<ListNode* , vector<ListNode*> , decltype(comp)> pq(comp) ;
         
-//         /* We iterate the list of LinkedLists and add the head
-//         * of every list to the heap (the heap becomes of size k)
-//         */
-//         for(auto &l : lists)
-//         {
-//             if(l) pq.push(l) ;
-//         }
+        /* We iterate the list of LinkedLists and add the head
+        * of every list to the heap (the heap becomes of size k)
+        */
+        for(auto &l : lists)
+        {
+            if(l) pq.push(l) ;
+        }
         
-//         ListNode dummy(-1) , *tail = &dummy ;
-//          // Simply start popping the nodes from the heap and keep adding them to the list;
-//         while( !pq.empty() ) {
-//              // Pop the top element and store it in t
-//             auto t = pq.top() ; 
-//             pq.pop() ;
+        ListNode dummy(-1) , *tail = &dummy ;
+         // Simply start popping the nodes from the heap and keep adding them to the list;
+        while( !pq.empty() ) {
+             // Pop the top element and store it in t
+            auto t = pq.top() ; 
+            pq.pop() ;
             
-//              // Add it to the list
-//             tail->next = t ; 
+             // Add it to the list
+            tail->next = t ; 
             
-//              // Move the tail to the newly added element
-//             tail = tail->next ; 
+             // Move the tail to the newly added element
+            tail = tail->next ; 
             
             
-//             // if there are more nodes attached to this node in the list
-//             // Push the next node of the list into the heap
-//             if(tail->next) pq.push(tail->next) ;
-//         }
-//         tail->next = NULL;
+            // if there are more nodes attached to this node in the list
+            // Push the next node of the list into the heap
+            if(tail->next) pq.push(tail->next) ;
+        }
+        tail->next = NULL;
         
         
-//         // We used the dummy to keep track of the start of the list.
-//         return dummy.next ;
+        // We used the dummy to keep track of the start of the list.
+        return dummy.next ;
         
         
         
