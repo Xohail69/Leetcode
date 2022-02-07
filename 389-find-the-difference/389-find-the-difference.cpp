d0468f97fd8e0ff4073f378d9ea0589176778447
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-02-07 16:02:42 +0530

    Time: 3 ms (78.44%), Space: 6.5 MB (87.58%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -1,12 +1,23 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        vector<int> h (26 , 0 ) ;
-        for(auto x : t ) h[x - 'a']++ ;
-        for(auto x : s ) h[x - 'a']-- ;
-        for(int i = 0 ; i < 26 ; i++  ) {
-            if( h[i]  ) return i + 'a' ;
+//         vector<int> h (26 , 0 ) ;
+//         for(auto x : t ) h[x - 'a']++ ;
+//         for(auto x : s ) h[x - 'a']-- ;
+//         for(int i = 0 ; i < 26 ; i++  ) {
+//             if( h[i]  ) return i + 'a' ;
+//         }
+//         return 'a' ;
+        
+        
+        char c ; 
+        int i = 0 ; 
+        for( ; i < s.size() ; i++ ) {
+            c += t[i] ;
+            c -= s[i] ;
         }
-        return 'a' ;
+        c += t[i] ;
+        return (int)c ;
+        
     }
 };
