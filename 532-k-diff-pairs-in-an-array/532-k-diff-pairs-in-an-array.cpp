3a81fd22e8d0981c1a614da63202f0d729bf3004
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-02-10 16:30:25 +0530

    Time: 45 ms (21.58%), Space: 13.3 MB (37.23%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
@@ -1,30 +1,37 @@
 class Solution {
 public:
     int findPairs(vector<int>& a, int k) {
-//         unordered_map<int,int> m ;
-//         for(auto x: a) m[x]++  ;
-//         int c =0;
-//         for(auto x: m){
-//         if(k==0) {    
-//             if(x.second>1)
-//             c++;
-//             }
-//             else if(m.find(x.first+k)!=m.end())
-//                 c++;            
-//             }
-//         return c ;
+        //M1 TC - O(n) and SC - O(N) 
+//         there are 2 cases :-
+
+//1. If k > 0, then in this case we just need to check wether the counter part exists or not. So, if we are iterating 'x + k' in our map, then we can increment our count
+
+//2.  If k == 0, then we just need to check if x is more then 1 or not [x > 1] in our map.
+
+// But another thing we need to note that here as we iterate from the array after doing counting. Then we will get "1" two times. So, to avoid this instead of iterating over the array, we will iterate over the keyset of this map, which will give us the unique no. i.e. (3,1,4,5).
         
         
+        unordered_map<int,int> m ;
+        for(auto x: a) m[x]++  ;
+        int c =0;
+        for(auto x: m){    
+        if ((!k and x.second > 1) or (k and m.count(x.first + k)))
+            c++ ;
         
-        
-        sort(a.begin() , a.end()) ;
-        set<int>s ;
-        for(auto i = a.begin() ; i != a.end() ; i++ ) {
-            if(binary_search( i+1 , a.end() , *i + k )){
-                s.insert(*i) ;
-            }
         }
-        return s.size() ;
+        return c ;
+        
+        
+        
+        //M2 TC , SC both O(NlogN)
+//         sort(a.begin() , a.end()) ;
+//         set<int>s ;
+//         for(auto i = a.begin() ; i != a.end() ; i++ ) {
+//             if(binary_search( i+1 , a.end() , *i + k )){
+//                 s.insert(*i) ;
+//             }
+//         }
+//         return s.size() ;
         
         
         
