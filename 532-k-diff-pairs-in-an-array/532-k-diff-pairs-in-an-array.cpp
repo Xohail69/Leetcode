9090e318b346ac22ed3728733286402c2cd4752f
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-02-10 17:12:43 +0530

    Time: 20 ms (73.48%), Space: 11.1 MB (92.84%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
@@ -11,19 +11,20 @@ public:
 // But another thing we need to note that here as we iterate from the array after doing counting. Then we will get "1" two times. So, to avoid this instead of iterating over the array, we will iterate over the keyset of this map, which will give us the unique no. i.e. (3,1,4,5).
         
         
-        unordered_map<int,int> m ;
-        for(auto x: a) m[x]++  ;
-        int c =0;
-        for(auto x: m){    
-        if ((!k and x.second > 1) or (k and m.count(x.first + k)))
-            c++ ;
+//         unordered_map<int,int> m ;
+//         for(auto x: a) m[x]++  ;
+//         int c =0;
+//         for(auto x: m){    
+//         if ((!k and x.second > 1) or (k and m.count(x.first + k)))
+//             c++ ;
         
-        }
-        return c ;
+//         }
+//         return c ;
         
         
         
-        //M2 TC , SC both O(NlogN)
+        //M2 Using BS 
+        //TC O(NlogN) SC O(1) {if sorting space is not counted}
 //         sort(a.begin() , a.end()) ;
 //         set<int>s ;
 //         for(auto i = a.begin() ; i != a.end() ; i++ ) {
@@ -32,6 +33,41 @@ public:
 //             }
 //         }
 //         return s.size() ;
+        
+        
+        //M3 usint Two Prs 
+        //TC O(nlogn) SC O(1) {if sorting space is not counted}
+        
+        
+        
+        
+    
+         int start = 0;
+    int end = 0;
+    int pairCount = 0;
+    sort(a.begin() , a.end()) ;
+    while(start < a.size() && end < a.size() ) {
+      if(start == end || a[start] + k > a[end]) end++;
+      else if(a[start] + k < a[end]) start++;
+      else {
+        pairCount++;
+        start++;
+        while(end < a.size() - 1 && a[end] == a[end + 1]) end++;
+        end++;
+      }
+    }
+    return pairCount;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
         
         
