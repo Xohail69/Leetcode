14cd4e89f24c5e75716ecbd199e28812b6ff4a04
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-02-14 12:06:17 +0530

    Time: 12 ms (53.08%), Space: 19 MB (15.07%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
@@ -4,66 +4,49 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     T''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''reeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    
+    int helper(TreeNode* root) {
+        if( !root ) return 0 ;
+         int left = maxDepth(root->left);
+        int right = maxDepth(root->right);
+        return max(left, right) + 1;
+     //or one liner
+        //   return max( helper(root->left) , helper(root->right) ) + 1 ;
+        
+        
+        
+        
+    }
     int maxDepth(TreeNode* root) {
-        //Using Recursion
-      // if ( !root ) return 0 ;
-      //   int left_depth = maxDepth(root->left) ;
-      //   int right_depth = maxDepth(root->right) ;
-      //    return (left_depth > right_depth) ? left_depth +1 :  right_depth +1;
+//         DFS
+// Time Complexity: O (n)
+// Space Complexity: O (h)
+       // return helper(root) ;
         
-    //above code can also be written in one line as 
- //return !root ? 0 : max(maxDepth(root->left) , maxDepth(root->right) ) + 1 ;
-        
-        
-        
-        
-        
-        //Using Level order traversal 
-        if(!root) return 0 ;
-        int ans = 0 ; 
+        //M2 using BFS
+// Time Complexity: O (n)
+// Space Complexity: O (n)
+        if( !root ) return 0 ;
         queue<TreeNode*> q ;
         q.push(root) ;
+        int depth = 0 ;
         while( !q.empty() ){
-            ans++ ;
-            for(int i =0 , n = q.size() ; i <  n  ; i++ )  {
-                TreeNode *p = q.front() ;
+            int n = q.size() ;
+            depth++ ;
+            while(n--){
+                TreeNode *t = q.front() ;
                 q.pop() ;
-                if(p->left) q.push(p->left) ;
-                if(p->right) q.push(p->right) ;
+                if(t->left) q.push(t->left) ;
+                if(t->right) q.push(t->right) ;
             }
         }
-        return ans ;
-        
-   
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        return depth ;
         
         
         
