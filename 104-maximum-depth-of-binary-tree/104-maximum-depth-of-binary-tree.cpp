1c7695c45f7681cf81c56beb054807b3cdee10dc
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-02-14 12:07:16 +0530

    Time: 31 ms (6.33%), Space: 18.9 MB (56.35%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
@@ -27,26 +27,26 @@ public:
 //         DFS
 // Time Complexity: O (n)
 // Space Complexity: O (h)
-       // return helper(root) ;
+        return helper(root) ;
         
         //M2 using BFS
 // Time Complexity: O (n)
 // Space Complexity: O (n)
-        if( !root ) return 0 ;
-        queue<TreeNode*> q ;
-        q.push(root) ;
-        int depth = 0 ;
-        while( !q.empty() ){
-            int n = q.size() ;
-            depth++ ;
-            while(n--){
-                TreeNode *t = q.front() ;
-                q.pop() ;
-                if(t->left) q.push(t->left) ;
-                if(t->right) q.push(t->right) ;
-            }
-        }
-        return depth ;
+//         if( !root ) return 0 ;
+//         queue<TreeNode*> q ;
+//         q.push(root) ;
+//         int depth = 0 ;
+//         while( !q.empty() ){
+//             int n = q.size() ;
+//             depth++ ;
+//             while(n--){
+//                 TreeNode *t = q.front() ;
+//                 q.pop() ;
+//                 if(t->left) q.push(t->left) ;
+//                 if(t->right) q.push(t->right) ;
+//             }
+//         }
+//         return depth ;
         
         
         
