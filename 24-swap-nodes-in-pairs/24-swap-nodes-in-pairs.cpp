be1b9f6278ac360962ad824dc32d2a389b0bafdf
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-02-16 21:51:36 +0530

    Time: 3 ms (70.43%), Space: 7.4 MB (92.21%) - LeetHub

--- 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cpp
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cpp
@@ -15,26 +15,76 @@ class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
         
-       
-        if( !head or !head->next ) return head ;
-        ListNode *p , *q , *t , *nhead ;
-        nhead = head->next ; 
-        p = head ;
-        while( 1 ){
-            q = p->next ; 
-            t = q->next ; 
-            q->next = p ; //Changing links 
-            if(!t or !t->next){ //!t is for even list length & !t->next is for odd length list 
-                p->next = t ; 
-                break ; 
-            }
-            p->next = t->next ; 
-            p = t ; 
+       //Iterative 
+//         if( !head or !head->next ) return head ;
+//         ListNode *p , *q , *t , *nhead ;
+//         nhead = head->next ; 
+//         p = head ;
+//         while( 1 ){
+//             q = p->next ; 
+//             t = q->next ; 
+//             q->next = p ; //Changing links 
+//             if(!t or !t->next){ //!t is for even list length & !t->next is for odd length list 
+//                 p->next = t ; 
+//                 break ; 
+//             }
+//             p->next = t->next ; 
+//             p = t ; 
             
+//         }
+//         return nhead ;
+    
+    
+        
+        
+        ///Recursive M1
+         if( !head or !head->next ) return head ;
+        ListNode *curr = head ;
+        ListNode *nex = NULL ; 
+        ListNode *prev = NULL  ;
+        int cunt = 0 ; 
+        while( curr and cunt < 2  ){
+            nex = curr->next ; 
+            curr->next = prev ; 
+            prev = curr  ;
+            curr = nex ; 
+            cunt++  ;
         }
-        return nhead ;
-    
-    
+        if( nex ){
+            head->next = swapPairs(nex) ;
+        }
+        return prev ; 
+        
+        
+        //M2
+//          if( !head or !head->next ) return head ;
+//         ListNode *t = head->next ;
+//         head->next = swapPairs(t->next) ;
+//         t->next = head ; 
+//         return t ; 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     
     }
 };
