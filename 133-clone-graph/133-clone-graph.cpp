b93167a5e057cf03c94ab6811465bc640f74c14a
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-02-23 23:04:32 +0530

    Time: 8 ms (65.22%), Space: 8.7 MB (71.30%) - LeetHub

--- 133-clone-graph/133-clone-graph.cpp
+++ 133-clone-graph/133-clone-graph.cpp
@@ -38,9 +38,44 @@ public:
     }
     
     Node* cloneGraph(Node* node) {
-        if(!node) return NULL ; 
-        unordered_map<Node* , Node* > map ; 
+//         if(!node) return NULL ; 
+//         unordered_map<Node* , Node* > map ; 
         
-        return dfs(node , map ) ; 
+//         return dfs(node , map ) ; 
+        
+        
+        
+        
+        //M2 BFS
+        
+        
+    unordered_map<Node*, Node*> copies;
+        
+          if (!node) {
+            return NULL;
+        }
+        Node* copy = new Node(node -> val, {});
+        copies[node] = copy;
+        queue<Node*> todo;
+        todo.push(node);
+        while (!todo.empty()) {
+            Node* cur = todo.front();
+            todo.pop();
+            for (Node* neighbor : cur -> neighbors) {
+                if (copies.find(neighbor) == copies.end()) {
+                    copies[neighbor] = new Node(neighbor -> val, {});
+                    todo.push(neighbor);
+                }
+                copies[cur] -> neighbors.push_back(copies[neighbor]);
+            }
+        }
+        return copy;
+    
+
+        
+        
+        
+        
+        
     }
 };
