4b33a4d3541ecd880a970303aef2d6362d499324
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-02-27 21:09:27 +0530

    Time: 20 ms (20.53%), Space: 17.5 MB (21.15%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -18,19 +18,22 @@ public:
         queue<pair<TreeNode*,int>> q ;
         q.push({root , 0 }) ;
         while( !q.empty() ){
-            int min = q.front().second ; 
+  
             int n = q.size() ; 
-            int first , last ; 
-            for(int i = 0 ; i < n ; i++ ) {
-                int cid = q.front().second -  min ;
+            int first  = q.front().second ; 
+            int last  = q.back().second ; 
+            
+             ans = max(ans , last - first + 1 ) ; 
+            
+      
+            while(n--) {
+                int cid = q.front().second -  first ;  //Ci=urrent index
                 TreeNode *t = q.front().first ; 
                 q.pop() ;
-                if(i == 0 ) first = cid ; 
-                if(i == n - 1 ) last = cid ; 
                 if(t->left ) q.push({t->left , 1LL*2*cid + 1 }) ;
                 if(t->right) q.push({t->right , 1LL*2*cid + 2 })  ; 
             }
-            ans = max(ans , last - first + 1 ) ; 
+          
             
         }
         return ans ; 
