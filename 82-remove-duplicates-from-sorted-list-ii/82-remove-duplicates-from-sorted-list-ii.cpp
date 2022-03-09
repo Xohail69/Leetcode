058ebea85d40e2a204e80c5cddd1a496e3c55604
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-03-09 13:10:09 +0530

    Time: 10 ms (58.79%), Space: 11.1 MB (91.86%) - LeetHub

--- 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.cpp
+++ 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.cpp
@@ -10,35 +10,89 @@
  */
 class Solution {
 public:
-    ListNode* create(map<int , int > &s){
-       // ListNode dummy(0) , *tail = &dummy ; 
-        auto x = s.begin() ;
-        while(x->second != 1 and x != s.end() ) x++ ;
-        if( x== s.end() ) return NULL ; 
-        ListNode *head = new ListNode(x->first , NULL ) ;
-        ListNode *t = head ;
-        ++x ;
-        for( ; x != s.end() ; x++ ){
-             while(x->second != 1 and x != s.end() ) x++ ;
-        if( x== s.end() ) return head ; 
-            ListNode *n =  new ListNode(x->first , NULL )  ;
-            t->next = n ; 
-            t = t->next ; 
-        }
-        return head ; 
-    }
+//     ListNode* create(map<int , int > &s){
+//        // ListNode dummy(0) , *tail = &dummy ; 
+//         auto x = s.begin() ;
+//         while(x->second != 1 and x != s.end() ) x++ ;
+//         if( x== s.end() ) return NULL ; 
+//         ListNode *head = new ListNode(x->first , NULL ) ;
+//         ListNode *t = head ;
+//         ++x ;
+//         for( ; x != s.end() ; x++ ){
+//              while(x->second != 1 and x != s.end() ) x++ ;
+//         if( x== s.end() ) return head ; 
+//             ListNode *n =  new ListNode(x->first , NULL )  ;
+//             t->next = n ; 
+//             t = t->next ; 
+//         }
+//         return head ; 
+//     }
     
     
     ListNode* deleteDuplicates(ListNode* head) {
         if( !head or !head->next ) return head ;
+        //Crappy sol using map
+//         map<int , int> s ; 
+//         ListNode* p = head ; 
+//         while(p){
+//            s[p->val]++ ; 
+//             p = p->next ;
+//         }
+//         return create(s) ;
         
-        map<int , int> s ; 
-        ListNode* p = head ; 
-        while(p){
-           s[p->val]++ ; 
-            p = p->next ;
+        
+    
+        
+        
+        
+        
+        //Best in O(n) & O(1)
+        ListNode sentinel(0 , head ) ;
+        ListNode *p = &sentinel  ; //Predecessor
+        while( head ){
+            // if it's a beginning of duplicates sublist 
+            // skip all duplicates
+            if(head->next and head->val == head->next->val){
+                 // move till the end of duplicates sublist
+                while(head->next and head->val == head->next->val )
+                    head = head->next ; 
+                
+                // skip all duplicates
+                p->next = head->next ;
+            }
+             // otherwise, move predecessor
+            else p = p->next ; 
+              // move forward
+            head = head->next ; 
         }
-        return create(s) ;
+        return sentinel.next ; 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
     }
 };
