83566f1f17e5d797151743beab7f7feba729800c
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-03-16 22:24:26 +0530

    Added solution - LeetHub

---
+++ Subsets - GFG/subsets.cpp
@@ -0,0 +1,74 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{   vector<vector<int>> ans ; 
+    vector<int> curr ; 
+    public:
+    void helper(vector<int> &a , int i , int n ){
+        if(i >= n ){
+            ans.push_back(curr) ;
+            return ; 
+        }
+        curr.push_back(a[i]) ;
+        helper(a , i+1 , n ) ;
+        curr.pop_back() ; 
+        helper(a , i+1 , n ) ;
+    }
+    
+    
+    vector<vector<int> > subsets(vector<int>& A)
+    {
+        //code here
+        ans = {} ; 
+        curr = {}  ;
+        int n = A.size() ; 
+        helper(A , 0 , n ) ; 
+        sort(ans.begin() , ans.end() ) ;
+        return ans ; 
+        
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin >> t;
+
+	while (t--)
+	{
+		int n, x;
+		cin >> n;
+
+		vector<int> array;
+		for (int i = 0; i < n; i++)
+		{
+			cin >> x;
+			array.push_back(x);
+		}
+        
+        
+        Solution ob;
+		vector<vector<int> > res = ob.subsets(array);
+
+		// Print result
+		for (int i = 0; i < res.size(); i++) {
+			for (int j = 0; j < res[i].size(); j++)
+				cout << res[i][j] << " ";
+			cout << endl;
+		}
+
+		
+	}
+
+	return 0;
+}  // } Driver Code Ends
