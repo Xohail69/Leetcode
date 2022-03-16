df3b52c0ef61e0ad75f521d9be87b36358d07410
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-03-16 22:10:08 +0530

    Time: 36 ms (5.05%), Space: 15.2 MB (81.35%) - LeetHub

--- 946-validate-stack-sequences/946-validate-stack-sequences.cpp
+++ 946-validate-stack-sequences/946-validate-stack-sequences.cpp
@@ -1,29 +1,56 @@
 class Solution {
 public:
     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
-        stack<int> s ; 
-        int  i = 0 , j = 0 ; 
-        int n = pushed.size() ; 
-        int m = popped.size() ;
-        while(j < m and i <n ){
+//         stack<int> s ; 
+//         int  i = 0 , j = 0 ; 
+//         int n = pushed.size() ; 
+//         int m = popped.size() ;
+//         while(j < m and i <n ){
          
-            while(i < n and pushed[i] != popped[j] ){
-             s.push(pushed[i++]) ;
-            }
-            if(pushed[i] == popped[j] ) s.push(pushed[i++]) ;
-           while(!s.empty() and s.top() == popped[j] ){
-                s.pop() ; j++ ;
-            }
+//             while(i < n and pushed[i] != popped[j] ){
+//              s.push(pushed[i++]) ;
+//             }
+//             if(pushed[i] == popped[j] ) s.push(pushed[i++]) ;
+//            while(!s.empty() and s.top() == popped[j] ){
+//                 s.pop() ; j++ ;
+//             }
             
             
-        }
-        if(i == n and j < m ){
-            while(!s.empty() and popped[j] == s.top() ){
+//         }
+//         if(i == n and j < m ){
+//             while(!s.empty() and popped[j] == s.top() ){
+//                 s.pop() ; j++  ;
+//             }
+//         }
+//         if(s.empty() and j == m ) return 1 ; 
+//         else return 0 ; 
+        
+        
+        
+        int n =  pushed.size() ; 
+        stack<int> s ; 
+         int j = 0 ; 
+        for(auto x : pushed ){
+            s.push(x) ; 
+            while( !s.empty() and j < n and s.top() == popped[j] ){
                 s.pop() ; j++  ;
             }
         }
-        if(s.empty() and j == m ) return 1 ; 
-        else return 0 ; 
+        return j == n ; 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
         
         
