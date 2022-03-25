ec1de0280ed3ff0ba18aa479ed3908d255375be0
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-03-25 22:16:09 +0530

    Time: 12 ms (23.24%), Space: 8.1 MB (33.00%) - LeetHub

---
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+   static bool comp(vector<int> &a , vector<int> &b){
+        return a[1]-a[0] > b[1]-b[0] ;
+    }
+    int twoCitySchedCost(vector<vector<int>>& costs) {
+//         int n = costs.size() ;
+//         sort(costs.begin() , costs.end() , comp ) ;
+//         int ans(0) ;
+//         for(int i = 0 ; i < n ; i++ ){
+//             ans += i >= n/2 ? costs[i][1] : costs[i][0] ;
+            
+//         }
+//         return ans ; 
+        
+        
+        
+//M2 
+        int n = costs.size() ; 
+            vector<int> costDiff(n) ;
+        int totalcityAcost = 0 ; 
+        int i = 0 ; 
+        for(auto &x : costs){
+            totalcityAcost += x[0] ;
+            costDiff[i++] = x[1] - x[0] ;
+        }
+        sort(costDiff.begin() , costDiff.end() ) ;
+        for(int i = 0 ; i < n/2  ; i++ ) {
+            totalcityAcost += costDiff[i] ;
+        }
+        return totalcityAcost ; 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+};
