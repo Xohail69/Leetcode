93103d6ce3d2567da4fb9209539f6719d3d3f74f
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-03-25 22:16:40 +0530

    Time: 4 ms (78.75%), Space: 7.7 MB (94.48%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -4,31 +4,31 @@ public:
         return a[1]-a[0] > b[1]-b[0] ;
     }
     int twoCitySchedCost(vector<vector<int>>& costs) {
-//         int n = costs.size() ;
-//         sort(costs.begin() , costs.end() , comp ) ;
-//         int ans(0) ;
-//         for(int i = 0 ; i < n ; i++ ){
-//             ans += i >= n/2 ? costs[i][1] : costs[i][0] ;
+        int n = costs.size() ;
+        sort(costs.begin() , costs.end() , comp ) ;
+        int ans(0) ;
+        for(int i = 0 ; i < n ; i++ ){
+            ans += i >= n/2 ? costs[i][1] : costs[i][0] ;
             
+        }
+        return ans ; 
+        
+        
+        
+// //M2 
+//         int n = costs.size() ; 
+//             vector<int> costDiff(n) ;
+//         int totalcityAcost = 0 ; 
+//         int i = 0 ; 
+//         for(auto &x : costs){
+//             totalcityAcost += x[0] ;
+//             costDiff[i++] = x[1] - x[0] ;
 //         }
-//         return ans ; 
-        
-        
-        
-//M2 
-        int n = costs.size() ; 
-            vector<int> costDiff(n) ;
-        int totalcityAcost = 0 ; 
-        int i = 0 ; 
-        for(auto &x : costs){
-            totalcityAcost += x[0] ;
-            costDiff[i++] = x[1] - x[0] ;
-        }
-        sort(costDiff.begin() , costDiff.end() ) ;
-        for(int i = 0 ; i < n/2  ; i++ ) {
-            totalcityAcost += costDiff[i] ;
-        }
-        return totalcityAcost ; 
+//         sort(costDiff.begin() , costDiff.end() ) ;
+//         for(int i = 0 ; i < n/2  ; i++ ) {
+//             totalcityAcost += costDiff[i] ;
+//         }
+//         return totalcityAcost ; 
         
         
         
