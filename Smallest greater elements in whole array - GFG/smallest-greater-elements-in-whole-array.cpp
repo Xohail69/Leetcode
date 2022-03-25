bdefcc31c7ef456500f4e944dd656ceb0003a28c
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-03-25 18:17:19 +0530

    Added solution - LeetHub

--- Smallest greater elements in whole array - GFG/smallest-greater-elements-in-whole-array.cpp
+++ Smallest greater elements in whole array - GFG/smallest-greater-elements-in-whole-array.cpp
@@ -33,24 +33,35 @@ int main()
 
 
 int* greaterElement(int a[], int n)
-{
-   set<int> s(a , a+n ) ;
-   int kabada = -10000000 ;
-   for(int i = 0  ; i < n ; i++ ){
-       auto it = s.find(a[i]) ;
-       it++ ;
-       if(it != s.end() ){
-           a[i] = *it ;
-       }
-       else a[i] =kabada ;
-   }
+{   //M1 best TC - O(nlogn ) & SC - O(n) ;
+//   set<int> s(a , a+n ) ;
+//   int kabada = -10000000 ;
+//   for(int i = 0  ; i < n ; i++ ){
+//       auto it = s.find(a[i]) ;
+//       it++ ;
+//       if(it != s.end() ){
+//           a[i] = *it ;
+//       }
+//       else a[i] =kabada ;
+//   }
    
    
-   return a ;
+//   return a ;
+        //
+        //M2 using unordered map 
+        vector<int>v(a , a+n ) ;
+        sort(v.begin() , v.end() ) ;
+        unordered_map<int,int> m ;
+        for(int i = 0 ; i < n -1 ; i++ )
+        m[v[i]] = v[i+1] ;
+        m[v[n-1]] = -10000000 ;
+        
+        for(int i = 0 ; i < n ; i++ ){
+            a[i] = m[a[i]] ;
+        }
         
         
-        
-        
+        return a ;
         
         
         
