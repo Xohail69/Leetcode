8d1a4540a9d5e5e736a7b533d0bb37a7ba7c4267
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-03-31 22:24:49 +0530

    Added solution - LeetHub

---
+++ Nodes at even distance - GFG/nodes-at-even-distance.cpp
@@ -0,0 +1,66 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    int countOfNodes(vector<int> graph[], int n)
+    {
+        // code here
+        
+        queue<int> q ; 
+        vector<int> dist(n+1 , 0 )  ;
+        vector<bool> visited(n+1 , false ) ;
+        q.push(1) ;
+        visited[1] = true ; 
+        while(!q.empty() ) {
+            int node = q.front()  ; 
+            q.pop() ;
+            int distance = dist[node] ;
+            for(int neighbour : graph[node] ){
+                if(!visited[neighbour]){
+                    q.push(neighbour) ;
+                    dist[neighbour] = distance + 1 ;
+                    visited[neighbour] = true ;
+                }
+            }
+        }
+        
+        int even (0) , odd(0) ;
+        for(int i = 1 ; i <= n  ; i++ ) {
+            if(dist[i] & 1 ) odd++ ;
+            else even++ ;
+        }
+        return even*(even-1)/2 + odd*(odd-1)/2 ;
+        
+        
+        
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        vector<int>graph[n+1];
+        for(int i=0;i<n-1;i++){
+            int u, v;
+            cin >> u >> v;
+            graph[u].push_back(v);
+            graph[v].push_back(u);
+        }
+        Solution ob;
+        cout<<ob.countOfNodes(graph, n)<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
