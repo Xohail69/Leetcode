a3a638d800389c586b284d702c1b36b7a41bfbe2
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-04-07 23:50:02 +0530

    Time: 3 ms (55.52%), Space: 7.8 MB (34.27%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -2,36 +2,29 @@ class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
         if(stones.size() == 1) return stones[0] ;
-        priority_queue<int> p(begin(stones) , end(stones) ) ;
-        while( p.size() > 1 ){
-            auto a = p.top() ; 
-            p.pop() ; 
-            auto b = p.top() ; 
-            p.pop()  ;
+        //Using PQ
+//         priority_queue<int> p(begin(stones) , end(stones) ) ;
+//         while( p.size() > 1 ){
+//             auto a = p.top() ; 
+//             p.pop() ; 
+//             auto b = p.top() ; 
+//             p.pop()  ;
             
-            if(a != b) p.push(a-b) ;
-        }
-        return p.empty() ? 0 : p.top() ;
-        
-        
-        
-//          priority_queue<int> pq(begin(A), end(A));
-//         while (pq.size() > 1) {
-//             int x = pq.top(); pq.pop();
-//             int y = pq.top(); pq.pop();
-//             if (x > y) pq.push(x - y);
+//             if(a != b) p.push(a-b) ;
 //         }
-//         return pq.empty() ? 0 : pq.top();
+//         return p.empty() ? 0 : p.top() ;
         
-//          multiset<int> s(begin(st), end(st));
-//   while (s.size() > 1) {
-//     auto w1 = *prev(s.end());
-//     s.erase(prev(s.end()));
-//     auto w2 = *prev(s.end());
-//     s.erase(prev(s.end()));
-//     if (w1 - w2 > 0) s.insert(w1 - w2);
-//   }
-//   return s.empty() ? 0 : *s.begin();
+        
+        //Using multiset 
+         multiset<int> s(begin(stones), end(stones));
+  while (s.size() > 1) {
+    auto x = *prev(s.end());
+    s.erase(prev(s.end()));
+    auto y = *prev(s.end());
+    s.erase(prev(s.end()));
+    if (x != y ) s.insert(x - y );
+  }
+  return s.empty() ? 0 : *s.begin();
         
     }
 };
