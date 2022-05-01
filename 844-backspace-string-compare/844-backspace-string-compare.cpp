c820bc582ec13e8feb64bf7e68859710d1667c87
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-05-01 18:28:03 +0530

    Time: 3 ms (43.16%), Space: 6.4 MB (39.15%) - LeetHub

---
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    
+#define ll                  long long
+#define loop(i,s,n) for(int i=s;i<n;++i)
+#define rloop(i,n,s) for(int i=n;i>=s;--i)
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define ins insert
+#define itoc(c) ((char)(((int)'0') + c))
+#define max(x, y) ((x > y) ? x : y)
+#define min(x, y) ((x < y) ? x : y)
+#define mid(s, e) (s + (e - s) / 2)
+const int MOD = 1000000007;
+const char nl = '\n';
+const int MX = 100001;
+typedef pair<int, int> pi;
+template <class T> T mceil(T a, T b) {if (a % b == 0) return a / b; else return a / b + 1;}
+template <class T> T mfloor(T a, T b) {if (a % b == 0) return a / b; else return a / b - 1;}
+#define db1(x) 						  cout<<#x<<"= "<<x<<'\n'
+#define db2(x,y) 					  cout<<#x<<"= "<<x<<", "<<#y<<"= "<<y<<'\n'
+#define db3(x,y,z) 				  cout<<#x<<"= "<<x<<", "<<#y<<"= "<<y<<", "<<#z<<"= "<<z<<'\n'
+// using namespace std;
+#define rep(i, begin, end)  for (__typeof(end) i = (begin) - ((begin) > (end)); i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
+#define p2darray(a, n, m)   rep(i, 0, n){rep(j, 0, m){cout<<a[i][j]<<" ";}cout<<endl;}
+#define dv(v)        {cout<<#v<<" = [" ; for (auto x : v) cout << x << " , "; cout <<" ]"<< nl ; }
+#define d2dv(v)       { cout<<#v<<":"<<nl ;  for (auto &x : v) { for (auto y : x) {	cout << y << " "; } cout << nl; }   }
+
+    string back(string s){
+        string ans ;
+        int h = 0 ; 
+       stack<char> stk ;
+        for(auto x : s ) stk.push(x) ; 
+        while(!stk.empty() ){
+            auto y = stk.top() ;
+            stk.pop() ;
+            if(y == '#') h++ ;
+            else if( h == 0 ) ans += y ;
+            else h-- ;
+        }
+        return ans ;
+    }
+    
+    bool backspaceCompare(string s, string t) {
+       string a = back(s) ;
+        string b = back(t) ; 
+       db2(a , b ) ;
+        return a==b ;
+    }
+};
