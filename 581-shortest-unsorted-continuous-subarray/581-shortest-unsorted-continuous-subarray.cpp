9c7b5f961c4bd4381cd269635346193d59ca7bc9
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-05-03 19:51:56 +0530

    Time: 30 ms (87.15%), Space: 28.2 MB (11.86%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
@@ -1,15 +1,34 @@
 class Solution {
 public:
     int findUnsortedSubarray(vector<int>& nums) {
-        vector<int> sorted(nums);
-        sort(sorted.begin(), sorted.end());
-        int n = nums.size(), i = 0, j = n - 1;
-        while (i < n && nums[i] == sorted[i]) {
-            i++;
-        }
-        while (j > i && nums[j] == sorted[j]) {
-            j--;
-        }
+        // vector<int> sorted(nums);
+        // sort(sorted.begin(), sorted.end());
+        // int n = nums.size(), i = 0, j = n - 1;
+        // while (i < n && nums[i] == sorted[i]) {
+        //     i++;
+        // }
+        // while (j > i && nums[j] == sorted[j]) {
+        //     j--;
+        // }
+        // return j + 1 - i;
+        
+        
+        
+        //M2
+           int n = nums.size();
+        vector<int> maxlhs(n);   // max number from left to cur
+        vector<int> minrhs(n);   // min number from right to cur
+        for (int i = n - 1, minr = INT_MAX; i >= 0; i--) minrhs[i] = minr = min(minr, nums[i]);
+        for (int i = 0,     maxl = INT_MIN; i < n;  i++) maxlhs[i] = maxl = max(maxl, nums[i]);
+
+        int i = 0, j = n - 1;
+        while (i < n && nums[i] <= minrhs[i]) i++;
+        while (j > i && nums[j] >= maxlhs[j]) j--;
+
         return j + 1 - i;
+        
+        
+        
+        
     }
 };
