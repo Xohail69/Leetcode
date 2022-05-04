f3cdfe06e62d666ff0a025486592f6dac772c7f4
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-05-04 19:29:15 +0530

    Time: 346 ms (7.16%), Space: 72.8 MB (26.85%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -3,31 +3,31 @@ public:
     int maxOperations(vector<int>& nums, int sum ) {
         
         
-   int n = nums.size()  , ans = 0 ;
-        unordered_map<int , int> m ;
-      for(auto x : nums ) m[x]++ ; 
-        for(auto& [k,v ] : m ) {
-              if(2*k == sum) ans += v / 2, v -= v/2*2; 
-            else if(v > 0 && m.count(sum-k) and  m[sum-k] > 0) {                
-                int pair_cnt = min(v, m[sum-k]);
-                ans += pair_cnt;
-                v -= pair_cnt;
-                m[sum-k] -= pair_cnt;   
-        }
-        }
+//    int n = nums.size()  , ans = 0 ;
+//         unordered_map<int , int> m ;
+//       for(auto x : nums ) m[x]++ ; 
+//         for(auto& [k,v ] : m ) {
+//               if(2*k == sum) ans += v / 2, v -= v/2*2; 
+//             else if(v > 0 && m.count(sum-k) and  m[sum-k] > 0) {                
+//                 int pair_cnt = min(v, m[sum-k]);
+//                 ans += pair_cnt;
+//                 v -= pair_cnt;
+//                 m[sum-k] -= pair_cnt;   
+//         }
+//         }
        
-return ans ; 
+// return ans ; 
         
         
         //Single traversal
-        // unordered_map<int, int> freq;
-        // int ans = 0;
-        // for(auto cur: nums){
-        //     int complement = sum - cur;
-        //     if(freq[complement] > 0){ans++; freq[complement]--;}
-        //     else freq[cur]++;
-        // }
-        // return ans; 
+        unordered_map<int, int> freq;
+        int ans = 0;
+        for(auto cur: nums){
+            int complement = sum - cur;
+            if(freq[complement] > 0){ans++; freq[complement]--;}
+            else freq[cur]++;
+        }
+        return ans; 
         
         
         
