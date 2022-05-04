4d22a12d17cc56eb5e74d9602761703bffb7cf86
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-05-04 19:50:00 +0530

    Added solution - LeetHub

--- Hungry Pizza Lovers - GFG/hungry-pizza-lovers.cpp
+++ Hungry Pizza Lovers - GFG/hungry-pizza-lovers.cpp
@@ -35,7 +35,19 @@ int main() {
 
 
 vector<int> permute(vector<vector<int>> &arr, int n)
-{
+{       
+    
+//     First we make an array of pairs in which we will store the sum of order time and making time in first and index of order +1 in second.
+
+// After that we will sort the array if first value is not equal than we will sort in ascending order of first value , if it is equal than we will sort in descending order of second value which is the index of the order.
+
+// Hence we will store the second value in a separate array and return it as the result.
+
+// Time complexity - O(nlog(n)) (because of sorting).
+
+// space complexity - O(n).
+    
+    
     vector<pair<int,int>> v  ;
         vector<int> ans ;
     int i = 1 ; 
