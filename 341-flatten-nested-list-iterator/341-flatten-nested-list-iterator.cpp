30c0b215ba209d98f526efb21b7aff68c1b06b48
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-05-08 22:27:51 +0530

    Time: 28 ms (22.13%), Space: 12.7 MB (98.12%) - LeetHub

--- 341-flatten-nested-list-iterator/341-flatten-nested-list-iterator.cpp
+++ 341-flatten-nested-list-iterator/341-flatten-nested-list-iterator.cpp
@@ -17,68 +17,68 @@
  */
 
 class NestedIterator {
-    private :
-       vector<int> ans ; int i = 0 ;
-        void flat(NestedInteger &x ){
-            if(x.isInteger()) 
-                ans.push_back(x.getInteger() ) ;
-            else {
-                for(auto &y : x.getList() )
-                    flat(y) ;
-            }
-        }
-    
-public:
-  NestedIterator(vector<NestedInteger> &nestedList) {
-        for(auto &x : nestedList ){
-            flat(x) ;
-        }
-    }
-    
-    int next() {
-        return ans[i++] ;
-    }
-    
-    bool hasNext() {
-        return i < ans.size() ;
-    }
-    
-    
-    
-    
-    
-//      stack<vector<NestedInteger>::iterator> Start, End; // stores iterators to start & end of nested lists
-//     NestedIterator(vector<NestedInteger> &nestedList) {
-//         Start.push(begin(nestedList)); // only storing the iterator to start of nestedList
-//         End.push(end(nestedList));     // required to detect if we reached end of nestedList
-//     }
-//     // will only return the next integer and increment the iterator
-//     int next() {
-//         return Start.top()++ -> getInteger();
-//     }
-//     /*  before next() is called, each time hasNext will make sure there exists a integer to return
-// 		If there exists, it will make sure that top of stack points to it.
-// 		If stack size becomes empty, all of nestedList is traversed
-// 	*/
-// 	    bool hasNext() {        
-//         while(size(Start)){			
-//             auto top = Start.top();            
-// 			// If it is end of one of nested list, pop that list, so we can iterate over the next one
-//             if(Start.top() == End.top()){
-//                 Start.pop(), End.pop();
-//                 continue;
-//             }                
-//             if(top -> isInteger()) break;
-// 			// if top is not integer, then it is definately a list
-// 			// First increment top of stack, so we know that all integers uptill(including top list) now have been traversed
-// 			// and then push the start iterator of top into stack, so that we traverse that one next
-//             Start.top()++;
-//             Start.push(begin(top -> getList()));  
-//             End.push(end(top -> getList()));
+//     private :
+//        vector<int> ans ; int i = 0 ;
+//         void flat(NestedInteger &x ){
+//             if(x.isInteger()) 
+//                 ans.push_back(x.getInteger() ) ;
+//             else {
+//                 for(auto &y : x.getList() )
+//                     flat(y) ;
+//             }
 //         }
-// 		// true will only be returned when stack top holds iterator of next integer
-//         return size(Start); 
+    
+// public:
+//   NestedIterator(vector<NestedInteger> &nestedList) {
+//         for(auto &x : nestedList ){
+//             flat(x) ;
+//         }
 //     }
+    
+//     int next() {
+//         return ans[i++] ;
+//     }
+    
+//     bool hasNext() {
+//         return i < ans.size() ;
+//     }
+    
+    
+    
+    
+    public :
+     stack<vector<NestedInteger>::iterator> Start, End; // stores iterators to start & end of nested lists
+    NestedIterator(vector<NestedInteger> &nestedList) {
+        Start.push(begin(nestedList)); // only storing the iterator to start of nestedList
+        End.push(end(nestedList));     // required to detect if we reached end of nestedList
+    }
+    // will only return the next integer and increment the iterator
+    int next() {
+        return Start.top()++ -> getInteger();
+    }
+    /*  before next() is called, each time hasNext will make sure there exists a integer to return
+		If there exists, it will make sure that top of stack points to it.
+		If stack size becomes empty, all of nestedList is traversed
+	*/
+	    bool hasNext() {        
+        while(size(Start)){			
+            auto top = Start.top();            
+			// If it is end of one of nested list, pop that list, so we can iterate over the next one
+            if(Start.top() == End.top()){
+                Start.pop(), End.pop();
+                continue;
+            }                
+            if(top -> isInteger()) break;
+			// if top is not integer, then it is definately a list
+			// First increment top of stack, so we know that all integers uptill(including top list) now have been traversed
+			// and then push the start iterator of top into stack, so that we traverse that one next
+            Start.top()++;
+            Start.push(begin(top -> getList()));  
+            End.push(end(top -> getList()));
+        }
+		// true will only be returned when stack top holds iterator of next integer
+        return size(Start); 
+    }
     
     
     
