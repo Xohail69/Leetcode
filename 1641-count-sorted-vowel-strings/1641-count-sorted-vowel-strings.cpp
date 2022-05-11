badd88820526b085f07c2799e3c295553ff5d42a
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-05-11 21:53:45 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (98.39%) - LeetHub

---
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -0,0 +1,70 @@
+class Solution {
+public:
+    int increment(int &a , int &e , int &i , int &o , int &u , int n ){
+        if(n == 1 ) return a+e+i+o+u ;
+//         a = number of strings that start with a
+// e = number of strings that start with e
+// i = number of strings that start with i
+// o = number of strings that start with o
+// u = number of strings that start with u
+        
+        
+        
+        a = a + e + i + o + u ; 
+        e = e + i + o + u ; 
+        i = i + o + u ; 
+        o = o + u ; 
+        return increment(a , e , i , o , u , n - 1 )  ;
+    }
+    
+    
+    int countVowelStrings(int n) {
+//         //M1 Here we can observe a pattern to this problem.
+// 		  a  e  i  o  u
+//     n=1   1  1  1  1  1  /a, e, i, o, u
+//     n=2   5  4  3  2  1  /a-> aa,ae,ai,ao,au | e-> ee,ei,eo,eu | i-> ii,io,iu | o-> oo,ou | u-> uu
+//     n=3   15 10 6  3  1
+// If we observe from last there will be only 1 element which will start with u. Every other element will have the count of previous count + next element count. As example
+// in n=2, i will be previous i(1) + count of next element, o(2) → 3
+// in n=3, e will be previous e(4) + count of next element, i(6) → 10
+              
+              
+        int a = 1 , e = 1 , i = 1 , o = 1 , u = 1 ; 
+        return increment(a , e , i , o ,u , n  )  ; 
+        
+        
+        //M2
+//           int a=1, e=1, i=1, o=1, u=1;
+        
+//         while(--n){
+//             o += u;
+//             i += o;
+//             e += i;
+//             a += e;
+//         }
+        
+//         return a+e+i+o+u;
+        
+        
+        
+        
+//         //M3 using dp
+//          vector<int> dp(5, 1);
+//         int ans = 0;
+        
+//         while(--n){
+//             for(int i=3; i>=0; i--){
+//                 dp[i] += dp[i+1];
+//             }
+//         }
+        
+//         for(auto x:dp) ans += x;
+        
+//         return ans;
+        
+        
+        // return (n + 1) * (n + 2) * (n + 3) * (n + 4) / 24;
+        
+        
+    }
+};
