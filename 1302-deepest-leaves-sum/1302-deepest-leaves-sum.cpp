e90f4486b9f58bf054c48bd5805dbe0a405404ac
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-05-15 17:05:10 +0530

    Time: 177 ms (15.41%), Space: 59.7 MB (94.18%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -11,25 +11,59 @@
  */
 class Solution {
 public:
+   int deepest = 0 , sum = 0 ;
+    int Deep_Baby(TreeNode* root , int depth = 0 ){
+      
+        
+        if(!root ) return 0 ;
+        if(!root->left and !root->right ) 
+           { if(depth == deepest ) sum += root->val ;
+            else if(depth > deepest ){ sum = root->val ; deepest = depth ;}
+        }
+        Deep_Baby(root->left , depth+1) ;
+        Deep_Baby(root->right , depth+1 ) ;
+        return sum ; 
+        
+    }
+    
+    
     int deepestLeavesSum(TreeNode* root) {
         
-          if( !root ) return 0 ;
-        queue<TreeNode*> q ;
-        int ans = 0 , d = 0 ;
-         q.push(root) ;
-        while( !q.empty() ){
-            int n = q.size() ;
-            ans = 0 ;
-            while(n--){
-                TreeNode *t = q.front() ;
-                 ans += t->val ;
-                q.pop() ;
-                if(t->left) q.push(t->left) ;
-                if(t->right) q.push(t->right) ;
-            }
-        }
+//           if( !root ) return 0 ;
+//         queue<TreeNode*> q ;
+//         int ans = 0 , d = 0 ;
+//          q.push(root) ;
+//         while( !q.empty() ){
+//             int n = q.size() ;
+//             ans = 0 ;
+//             while(n--){
+//                 TreeNode *t = q.front() ;
+//                  ans += t->val ;
+//                 q.pop() ;
+//                 if(t->left) q.push(t->left) ;
+//                 if(t->right) q.push(t->right) ;
+//             }
+//         }
+//         return ans ;
         
-
-        return ans ;
+        return Deep_Baby(root ) ;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 };
