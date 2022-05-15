db3d1f692e488e0ec3f40aa253c43e8e489770d1
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-05-15 16:31:15 +0530

    Time: 174 ms (16.82%), Space: 61.8 MB (43.30%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -15,36 +15,21 @@ public:
         
           if( !root ) return 0 ;
         queue<TreeNode*> q ;
-        q.push(root) ;
-        int depth = 0 ;
-        while( !q.empty() ){
-            int n = q.size() ;
-            depth++ ;
-            while(n--){
-                TreeNode *t = q.front() ;
-                q.pop() ;
-                if(t->left) q.push(t->left) ;
-                if(t->right) q.push(t->right) ;
-            }
-        }
         int ans = 0 , d = 0 ;
          q.push(root) ;
         while( !q.empty() ){
             int n = q.size() ;
-            d++ ;
-            
+            ans = 0 ;
             while(n--){
                 TreeNode *t = q.front() ;
-                if(d == depth ) ans += t->val ;
+                 ans += t->val ;
                 q.pop() ;
                 if(t->left) q.push(t->left) ;
                 if(t->right) q.push(t->right) ;
             }
         }
         
-        
-        
-        
+
         return ans ;
     }
 };
