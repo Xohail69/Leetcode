3ea9614962e33c4c90b10b49cdb2eafee77a6c78
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-05-20 16:15:17 +0530

    Time: 4 ms (25.36%), Space: 6.6 MB (5.02%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -19,8 +19,20 @@ public:
     }
     
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m , vector<int> (n , 0 )) ;
-         solve(0 , 0 , m , n , dp ) ; 
+        //Memoization
+      //  vector<vector<int>> dp(m , vector<int> (n , 0 )) ;
+         //solve(0 , 0 , m , n , dp ) ; 
+        
+        
+        //Tabulation
+         vector<vector<int>> dp(m , vector<int> (n , 1 )) ;
+        for(int i = m - 2 ; i >= 0 ; i-- ){
+            for(int j = n -2 ; j>=0 ; j-- ){
+            dp[i][j] = dp[i+1][j] + dp[i][j+1] ;
+            }
+        }
+        
+        
         return dp[0][0] ;
         
     }
