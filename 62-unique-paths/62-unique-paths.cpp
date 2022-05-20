c7d2a56796ecd3ad55cc92379174268f9656d266
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-05-20 16:25:04 +0530

    Time: 4 ms (25.36%), Space: 6 MB (74.55%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -25,15 +25,41 @@ public:
         
         
         //Tabulation
-         vector<vector<int>> dp(m , vector<int> (n , 1 )) ;
-        for(int i = m - 2 ; i >= 0 ; i-- ){
-            for(int j = n -2 ; j>=0 ; j-- ){
-            dp[i][j] = dp[i+1][j] + dp[i][j+1] ;
-            }
+//          vector<vector<int>> dp(m , vector<int> (n , 1 )) ;
+//         for(int i = m - 2 ; i >= 0 ; i-- ){
+//             for(int j = n -2 ; j>=0 ; j-- ){
+//             dp[i][j] = dp[i+1][j] + dp[i][j+1] ;
+//             }
+//         }
+        
+//         return dp[0][0] ;
+        
+        
+        
+        
+        //M3 using combinatorics
+//         We start at (0, 0) cell and move to (m-1, n-1) cell.
+// We need to make m-1 down-moves and n-1 right-moves to reach the destination cell.
+// Thus, we need to perform a total number of m+n-2 moves.
+// At each cell along the path, we can choose either the right-move or down-move and we need to find the number of unique combinations of these choices (which eventually leads to unique paths).
+// This is nothing but calculating the number of different ways to choose m-1 down-moves and n-1 right-moves from a total of m+n-2 moves. Mathematically, this can be represented as -
+        // (m+n-2)C(n-1) = (m+n-2)C(m-1)
+        
+        int N = n + m - 2 ;
+        int r = m - 1 ;
+        double ans = 1 ;
+        for(int i = 1 ; i <=r ; i++ ){
+            ans = ans*(N - r + i )/i ;
         }
+        return (int)ans ;
         
         
-        return dp[0][0] ;
+        
+        
+        
+        
+        
+        
         
     }
 };
