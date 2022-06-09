8157166e5b5e31e2ab7a6a65c48d2f0135a52c1e
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-06-09 20:26:10 +0530

    Time: 43 ms (5.14%), Space: 15.5 MB (93.11%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,17 +1,34 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        int i = 0  , j = nums.size()- 1;
-        vector<int>ans(2) ; 
-        while(i <= j ){
-            int tar = nums[i] + nums[j] ; 
-            if(tar == target){
-                ans[0] = i+1 ;
-                ans[1] = j+1 ; break ;
-            }
-            else if(tar < target ) i++ ;
-            else j-- ;
+   int bs(vector<int> &v ,int l , int h , int x){
+        int mid ;
+        while(l<=h){
+            mid = l + (h-l)/2 ;
+            if(v[mid]==x) return mid ;
+            else if(v[mid] < x) l = mid + 1 ;
+            else h = mid - 1 ;
         }
-        return ans ;
+        return -1 ;
+    }
+    
+    vector<int> twoSum(vector<int>& n, int target) {
+        //M1 using Two pointer 
+        
+//         int i = 0 , j = n.size()-1 ;
+//         while(j>i){
+//             int sum = n[i]+n[j] ;
+//             if( sum == target ) return {i+1 , j+1};
+//             else if( sum<target ) i++  ;
+//                 else j-- ;
+//         }
+//         return n ;
+        
+        //M2 using Binary Search 
+        
+        for(int i = 0 ; i< n.size() ; i++){
+            int j = bs(n , i+1 , n.size()-1 , target - n[i]) ;
+            if (j != -1 ) return {i+1 , j+1 } ;
+        }
+        return n ;
     }
 };
