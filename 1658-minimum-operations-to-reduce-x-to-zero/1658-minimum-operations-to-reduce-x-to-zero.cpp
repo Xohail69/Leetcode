3e20997dfb915c977bf7766f4085e3fe1b80a44b
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-06-11 18:41:03 +0530

    Time: 368 ms (45.11%), Space: 98.6 MB (71.00%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -2,11 +2,25 @@ class Solution {
 public:
     int minOperations(vector<int>& nums , int x) {
       
+        
+//         main point is that if successfully remove elements such that sum of removed elements is X , then
+//  1. sum of remaining array elements will be (sum-X)
+// 2.  remaing array will be subarray of original array.
+//         So , now our task is find subaarray whose sum is (sum - X) and its length is maximum ( because we want to remove minimum number of elements , so we will have to maximise remianing element ) .
+
+// TC - O(N)
+// SC - O(1)
+        
+        
+        
         if(nums[0] > x  and nums.back() > x ) return -1 ; 
         int sum =  accumulate(nums.begin() , nums.end() , 0 ) ;
         if(sum < x ) return -1 ; 
-       int target = sum - x ; 
+       int target = sum - x ; //We have to find max len subarray whose sum equals target
         if(target == 0 ) return nums.size()  ;
+        
+        
+        //Using sliding window 
         int l = 0  , n = nums.size() , r = 0 , t = 0 , ans = 0 ; 
         while(r < n ){
             t += nums[r]  ;
