29e09935fa5fa6d5a95f62f2a65f88bbb11cb711
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-06-14 12:26:08 +0530

    Time: 3 ms (52.41%), Space: 5.9 MB (30.20%) - LeetHub

--- 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.cpp
+++ 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.cpp
@@ -2,11 +2,20 @@ class Solution {
 public:
     int trailingZeroes(int n) {
         // https://leetcode.com/problems/factorial-trailing-zeroes/discuss/52470/4-lines-4ms-C%2B%2B-Solution-with-Explanations
-        int ans(0) ; 
-        for(long long j = 5 ; n / j > 0 ; j *= 5 ){
-            ans += n/ j ;
-        }
+//         int ans(0) ; 
+//         for(long long j = 5 ; n / j > 0 ; j *= 5 ){
+//             ans += n/ j ;
+//         }
         
-        return ans  ;
+//         return ans  ;
+        
+        
+        
+          int res=0;
+        while(n){
+            n/=5;
+            res+=n;
+        }
+        return res;
     }
 };
