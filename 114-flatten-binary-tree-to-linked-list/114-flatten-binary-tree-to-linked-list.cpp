f54b7ef451924f2b7a975cc6c128004bc1c97d8d
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-07-28 00:22:48 +0530

    Time: 24 ms (6.36%), Space: 12.7 MB (78.06%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
@@ -11,21 +11,34 @@
  */
 class Solution {
 public:
+    TreeNode *head = NULL ; 
+    void rev_pre(TreeNode *root){
+        if(root->right ) rev_pre(root->right) ; 
+        if(root->left ) rev_pre(root->left ) ; 
+        root->left = NULL ;
+        root->right = head ; 
+        head = root ; 
+    }
     void flatten(TreeNode* root) {
+        //https://leetcode.com/problems/flatten-binary-tree-to-linked-list/discuss/1207642/JS-Python-Java-C%2B%2B-or-Simple-O(1)-Space-and-Recursive-Solutions-w-Explanation
+        //M1  using Morris Traversal 
+        //TC - O(n) & SC - O(1) 
+        // auto curr = root ; 
+        // while( curr ){
+        //     if(curr->left){
+        //     auto runner =  curr->left  ; 
+        //     while(runner->right) runner = runner->right ; 
+        //     runner->right = curr->right ; 
+        //     curr->right = curr->left ; 
+        //     curr->left = NULL ; 
+        //     }
+        //        curr = curr->right ; 
+        // }
         
-        auto curr = root ; 
-        while( curr ){
-            if(curr->left){
-            auto runner =  curr->left  ; 
-            while(runner->right) runner = runner->right ; 
-            runner->right = curr->right ; 
-            curr->right = curr->left ; 
-            curr->left = NULL ; 
-            }
-               curr = curr->right ; 
-        }
+        //M2 using reverse preorder traversal 
         
-       
+        //TC - O(n) & SC - O(n) recursion stack  
+       if(root ) rev_pre(root) ; 
         
     }
 };
