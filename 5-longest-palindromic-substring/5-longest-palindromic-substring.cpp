ff4724668886d96d3f5ea9703fb7707bcedd86cb
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-07-28 20:13:29 +0530

    Time: 196 ms (55.74%), Space: 8 MB (69.33%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -56,30 +56,83 @@ public:
         
         
         //M2 expand around corner 
-        if(s.size() == 1 ) return s ; 
-        if(s.size() == 2 ){
-            if(s[0] == s[1] ) return s ;
+//         if(s.size() == 1 ) return s ; 
+//         if(s.size() == 2 ){
+//             if(s[0] == s[1] ) return s ;
             
-        }
-        int start = 0 , end = 0 ; 
-        for(int i = 0 ; i < s.size() ; i++ ){
-            int l1 = expand_around_center(s , i , i ) ; 
-            int l2 = expand_around_center(s , i , i + 1 ) ; 
-            int len = max(l1 , l2 ) ; 
-            if(len > end - start ) {
-                start = i - (len - 1 ) / 2 ; 
-                end = i + len / 2 ; 
-            }
+//         }
+//         int start = 0 , end = 0 ; 
+//         for(int i = 0 ; i < s.size() ; i++ ){
             
-        }
+//             //ex - "bab"
+//             int l1 = expand_around_center(s , i , i ) ; 
+            
+            
+//             //ex- "baab"
+//             int l2 = expand_around_center(s , i , i + 1 ) ; 
+//             int len = max(l1 , l2 ) ; 
+//             if(len > end - start ) {
+//                 start = i - (len - 1 ) / 2 ; 
+//                 end = i + len / 2 ; 
+//             }
+            
+//         }
         // string ans ;
         // for(int i = start ; i < end + 1 ; i++ ){
         //     ans += s[i] ;
         // }
         // return ans ;
-        return s.substr(start , end - start + 1 ) ; 
+        // return s.substr(start , end - start + 1 ) ; 
         
         
+        //M3 using DP in O(n^2) both 
+//         https://leetcode.com/problems/longest-palindromic-substring/discuss/900639/Python-Solution-%3A-with-detailed-explanation-%3A-using-DP
+       
+        int n  = s.size() ; 
+        
+         // dp[i][j] will be false if substring
+    // s[i..j] is not palindrome.
+    // Else dp[i][j] will be true
+        bool dp[n][n] ; 
+        memset(dp , 0 , sizeof(dp) ) ; 
+        int maxl = 1 ; //All subs of length 1 are pali
+        for(int i = 0 ; i < n ; i++ )
+                dp[i][i] = 1 ; 
+
+     // check for sub-string of length 2.
+        int start = 0 ; 
+        for(int i = 0  ; i < n - 1 ; i ++ ) {
+            if(s[i] == s[i+1] ) {
+                dp[i][i+1] = 1 ;
+                start = i ; 
+                maxl = 2 ;
+            }
+        }
+        
+         // Check for lengths greater than 2.
+    // k is length of substring
+    for(int k = 3 ; k <= n ; k++ ){
+        //Fix starting index ; 
+        for(int i = 0 ; i < n - k + 1 ; i++ ) {
+            // Get the ending index of substring from
+            // starting index i and length k
+            int j = i + k - 1;
+            
+            //check for sub from ith index to jth index iff s[i+1] to 
+            // s[j-1] is a pali 
+            if(dp[i+1][j-1] and s[i] == s[j] ){
+                dp[i][j] = 1 ; 
+                if(k > maxl ){
+                    start = i ; 
+                    maxl = k ;
+                }
+            }
+        }
+    }
+   
+    return s.substr(start , maxl ) ; 
+    
+    
         
         
         
