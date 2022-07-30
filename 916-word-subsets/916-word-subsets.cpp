f2031b301bae73b5c9e87b674542badb757d1c84
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-07-30 22:05:07 +0530

    Time: 332 ms (47.87%), Space: 57.4 MB (80.67%) - LeetHub

--- 916-word-subsets/916-word-subsets.cpp
+++ 916-word-subsets/916-word-subsets.cpp
@@ -25,7 +25,7 @@ public:
     }
     vector<string> wordSubsets(vector<string>& A , vector<string>& B ) {
         
-
+            //M1
 //         vector<string> ans ; 
 //         vector<int> f(26 , 0  ) ; 
         
@@ -43,17 +43,20 @@ public:
         
         
         
-        
+        //M2
 //            vector<string> ans ;
 //         vector<int>count(26) , temp(26) ; 
 //         for(auto &x : words2 ){
 //             temp = counter(x) ; 
+        //temp will act as frequency table of every word of word2 
 //             for(int i = 0 ;  i < 26 ; i++ ){
 //                 count[i] = max(count[i] , temp[i] ) ; 
 //             }
 //         }
         
 //         for(auto &y : words1 ){
+        
+        //      
 //             temp = counter(y) ; 
 //             int i = 0 ; 
 //             for( ; i < 26 ; i++ ) {
@@ -65,7 +68,7 @@ public:
         
 //         return ans ; 
         
-        
+        //M3 using STL
         string unit = "";
         for (auto& v : B) {
             string tmp;
