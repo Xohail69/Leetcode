24e7e04b01afb0529c0bc9df9817e6273f8972a4
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-07-30 19:05:35 +0530

    Time: 226 ms (80.23%), Space: 102.4 MB (65.17%) - LeetHub

--- 916-word-subsets/916-word-subsets.cpp
+++ 916-word-subsets/916-word-subsets.cpp
@@ -13,28 +13,53 @@ public:
         }
         return 1 ; 
     }
+    vector<int> counter(string &s ){
+        vector<int>h(26 , 0 ) ; 
+        for(auto x:s ) 
+            h[x-'a']++ ;
+        return h ; 
+    }
     vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
         
-        int teri_maaki_choot = 0 ; 
-        vector<string> ans ; 
-        vector<int> f(26 , 0  ) ; 
+
+//         vector<string> ans ; 
+//         vector<int> f(26 , 0  ) ; 
         
-        for(auto &s : words2 ){
-            for(char i = 'a' ; i < 'z' ; i++ ){
-                f[i - 'a' ] = max(f[i - 'a' ] , count(s , i ) ) ; 
+//         for(auto &s : words2 ){
+//             for(char i = 'a' ; i < 'z' ; i++ ){
+//                 f[i - 'a' ] = max(f[i - 'a' ] , count(s , i ) ) ; 
+//             }
+//         }
+        
+//         for(auto &x : words1 ){
+//             if(is_subset(x , f ) )
+//                 ans.push_back(x ) ; 
+//         }
+//         return ans ; 
+        
+        
+        
+        
+           vector<string> ans ;
+        vector<int>count(26) , temp(26) ; 
+        for(auto &x : words2 ){
+            temp = counter(x) ; 
+            for(int i = 0 ;  i < 26 ; i++ ){
+                count[i] = max(count[i] , temp[i] ) ; 
             }
         }
         
-        for(auto &x : words1 ){
-            if(is_subset(x , f ) )
-                ans.push_back(x ) ; 
+        for(auto &y : words1 ){
+            temp = counter(y) ; 
+            int i = 0 ; 
+            for( ; i < 26 ; i++ ) {
+                if(temp[i] < count[i] ) 
+                    break ;
+            }
+            if(i == 26 ) ans.push_back(y) ; 
         }
+        
         return ans ; 
-        
-        
-        
-        
-        
         
         
         
