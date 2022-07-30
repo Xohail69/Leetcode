6bcc6d6886c441e67764d843557f2aebdc0e8158
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-07-30 21:55:17 +0530

    Time: 312 ms (53.26%), Space: 57.4 MB (80.67%) - LeetHub

--- 916-word-subsets/916-word-subsets.cpp
+++ 916-word-subsets/916-word-subsets.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    #define db1(x) 						  cout<<#x<<"= "<<x<<'\n'
+#define db2(x,y) 					  cout<<#x<<"= "<<x<<", "<<#y<<"= "<<y<<'\n'
+#define db3(x,y,z) 				  cout<<#x<<"= "<<x<<", "<<#y<<"= "<<y<<", "<<#z<<"= "<<z<<'\n'
+    
     int count(string &s , char c ){
         int cunt =0  ; 
         for(auto x : s )
@@ -19,7 +23,7 @@ public:
             h[x-'a']++ ;
         return h ; 
     }
-    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+    vector<string> wordSubsets(vector<string>& A , vector<string>& B ) {
         
 
 //         vector<string> ans ; 
@@ -40,27 +44,46 @@ public:
         
         
         
-           vector<string> ans ;
-        vector<int>count(26) , temp(26) ; 
-        for(auto &x : words2 ){
-            temp = counter(x) ; 
-            for(int i = 0 ;  i < 26 ; i++ ){
-                count[i] = max(count[i] , temp[i] ) ; 
+//            vector<string> ans ;
+//         vector<int>count(26) , temp(26) ; 
+//         for(auto &x : words2 ){
+//             temp = counter(x) ; 
+//             for(int i = 0 ;  i < 26 ; i++ ){
+//                 count[i] = max(count[i] , temp[i] ) ; 
+//             }
+//         }
+        
+//         for(auto &y : words1 ){
+//             temp = counter(y) ; 
+//             int i = 0 ; 
+//             for( ; i < 26 ; i++ ) {
+//                 if(temp[i] < count[i] ) 
+//                     break ;
+//             }
+//             if(i == 26 ) ans.push_back(y) ; 
+//         }
+        
+//         return ans ; 
+        
+        
+        string unit = "";
+        for (auto& v : B) {
+            string tmp;
+            sort(v.begin(), v.end());
+            set_union(v.begin(), v.end(), unit.begin(), unit.end(), back_inserter(tmp));
+            // db2(tmp , unit ) ; 
+            swap(unit, tmp);
+            // db2(tmp , unit ) ; 
+        }
+        vector<string> res;
+        for (auto a : A) {
+            string t = a;
+            sort(t.begin(), t.end());
+            if (includes(t.begin(), t.end(), unit.begin(), unit.end())) {
+                res.push_back(a);
             }
         }
-        
-        for(auto &y : words1 ){
-            temp = counter(y) ; 
-            int i = 0 ; 
-            for( ; i < 26 ; i++ ) {
-                if(temp[i] < count[i] ) 
-                    break ;
-            }
-            if(i == 26 ) ans.push_back(y) ; 
-        }
-        
-        return ans ; 
-        
+        return res;
         
         
         
