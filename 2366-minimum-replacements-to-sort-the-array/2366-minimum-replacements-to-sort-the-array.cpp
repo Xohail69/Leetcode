c39d264d0ca1458068bbcbf2285221948b442317
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-08-10 19:55:38 +0530

    Time: 144 ms (75.00%), Space: 54.8 MB (75.00%) - LeetHub

--- 2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.cpp
+++ 2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.cpp
@@ -1,37 +1,23 @@
 class Solution {
 public:
     typedef long long int ll ;
-    long long minimumReplacement(vector<int>& nums) {
-//         int ans(0)  ;
-//         int n=a.size() , next = 1e9 + 7 ; 
-//         for(int j = n - 1 ; j >= 0 ; j-- ) {
-//             if(a[j] <= next ){
-//                 next = a[j] ; 
-//                 continue ; 
-//             }
-//             ll parts = ceil(a[j]/(double)next ) ; 
-//             ans += parts - 1 ; 
-//             next = parts ? a[j]/parts : 0  ; 
-//         }
-//         return ans ; 
-        
-        
-        
-         ll ans = 0;
-        int n = nums.size(), nxt = 1e9+7;
-        for (int j = n-1; j >= 0; j --) {
-            if (nums[j] <= nxt) {
-                nxt = nums[j];
-                continue;
+    long long minimumReplacement(vector<int>& a) {
+        ll ans(0)  ;
+        ll n=a.size() , next = 1e9 + 7 ; 
+        for(int j = n - 1 ; j >= 0 ; j-- ) {
+            if(a[j] <= next ){
+                next = a[j] ; 
+                continue ; 
             }
-            ll parts = ceil(nums[j]/(double)nxt);
-            
-            ans += parts-1;            
-            nxt = nums[j] / parts;;
+            ll parts = ceil(a[j]/(double)next ) ; 
+            ans += parts - 1 ; 
+            next =  a[j]/parts ; 
         }
-        return ans;
+        return ans ; 
         
         
+        
+       
         
         
         
