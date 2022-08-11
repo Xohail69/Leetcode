9d13f6243a6a32d984a484b8ee6398f67b5bc6eb
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-08-11 11:21:30 +0530

    Time: 26 ms (23.50%), Space: 21.7 MB (30.71%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -17,12 +17,48 @@ public:
         v.push_back(root->val) ; 
         helper(root->right , v ) ; 
     }
+    
+    
+    bool rec(TreeNode *root , TreeNode *min , TreeNode* max ){
+        if(!root ) return 1 ; 
+       
+        if( (min and root->val <= min->val) or (max and root->val >= max->val) )
+            return 0 ; 
+        
+        return rec(root->left , min , root) and rec(root->right , root , max ) ;
+    }
+    
+    bool solve(TreeNode *root , TreeNode* &prev){
+        if( !root ) return 1 ; 
+        if( !solve(root->left , prev ) ) return 0  ;
+        
+        if(prev and prev->val >= root->val ) 
+            return 0 ; 
+        prev = root ; 
+        return solve(root->right , prev ) ; 
+        
+        
+    }
+    
     bool isValidBST(TreeNode* root) {
-        vector<int> v ; 
-        helper(root , v ) ; 
-        for(int i = 0 ;  i < v.size() - 1 ;i ++ ){
-            if(v[i] >= v[i+1] ) return 0 ;
-        } 
-        return 1 ; 
+        //M1 using Inorder traversal & storing it in a vec
+        // vector<int> v ; 
+        // helper(root , v ) ; 
+        // for(int i = 0 ;  i < v.size() - 1 ;i ++ ){
+        //     if(v[i] >= v[i+1] ) return 0 ;
+        // } 
+        // return 1 ; 
+        
+        
+        //M2 using recursion 
+        // return rec(root , NULL , NULL ) ; 
+        
+        
+        
+        //M3 using rec & using prev Pr
+        TreeNode* prev = NULL ; 
+        return solve(root , prev ) ; 
+        
+   
     }
 };
