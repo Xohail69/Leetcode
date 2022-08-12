d3912fd6f27d4c19615b6aa3c55f5329407d4423
Xohail69 <92709773+Xohail69@users.noreply.github.com>
2022-08-12 20:25:09 +0530

    Time: 34 ms (86.93%), Space: 23.4 MB (12.34%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -10,21 +10,27 @@
 
 class Solution {
 public:
-    TreeNode* helper(TreeNode *root , TreeNode* p , TreeNode* q ){
-        if(!root ) return NULL ; 
-        if(root == p or root == q ) return root ; 
-        auto left = helper(root->left , p , q )  ;
-        auto right = helper(root->right , p , q ) ;
+    TreeNode* helper(TreeNode *root , int min , int max ){
+//         if(!root ) return NULL ; 
+//         if(root == p or root == q ) return root ; 
+//         auto left = helper(root->left , p , q )  ;
+//         auto right = helper(root->right , p , q ) ;
    
-         return left and right ? root : left ? left : right ; 
-     
+//          return left and right ? root : left ? left : right ; 
+        if(!root ) return root ; 
+        int node = root->val  ; 
+        if( min <= node and node <= max   ) return root ; 
+        if( node < min  ) return helper(root->right , min , max ) ; 
+        if(node > max ) return helper(root->left , min , max ) ; 
+        return NULL ;
          
     }
     
     
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        
-        return helper(root , p , q ) ; 
+        int maxi = max(p->val , q->val ) ; 
+        int mini = min(p->val , q->val ) ; 
+        return helper(root , mini , maxi  ) ; 
         
         
         
