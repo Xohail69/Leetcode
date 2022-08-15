07e482c3dcaa4165949e897ef26f5d32d9e0c27f
Sohail Rayeen <92709773+Xohail69@users.noreply.github.com>
2022-08-15 19:53:06 +0530

    Time: 3 ms (95.35%), Space: 10.5 MB (98.45%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.cpp
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -2,14 +2,23 @@ class Solution {
 public:
     void solve(int ix  , vector<int> &a , int target , vector<int> &v ,  vector<vector<int>>&ans){
         if(target == 0 ) {
+//base case
+        //when the combination ds is carrying is a valid one add it into th ans
             ans.push_back(v) ; 
             return ; 
         }
         for(int i = ix ; i < a.size() ; i++ ){
+            
+              //to avoid picking up the same elements and thus avoiding duplicates (no picking of element)
             if(i > ix and a[i] == a[i-1] ) continue ; 
+            
+            
+            // no point of it to add it to the ans if the  candidate at ith element exceeds the target no picking up hence break 
             if(a[i] > target ) break ; 
+            
+            
             v.push_back(a[i]) ; 
-            solve(i + 1 , a , target - a[i] , v , ans ) ; 
+            solve(i + 1 , a , target - a[i] , v , ans ) ; //call for next  index element
             v.pop_back() ; 
             
         }
@@ -27,3 +36,5 @@ public:
         return ans ; 
     }
 };
+
+
