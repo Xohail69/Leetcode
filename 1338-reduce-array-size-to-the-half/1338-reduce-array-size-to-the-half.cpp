2e145f99dc54e5d1c49444ce0a53c52f6c300ed0
Sohail Rayeen <92709773+Xohail69@users.noreply.github.com>
2022-08-19 00:23:18 +0530

    Time: 361 ms (46.41%), Space: 77.8 MB (86.06%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -1,28 +1,87 @@
 class Solution {
 public:
     int minSetSize(vector<int>& arr) {
-//         map<int,int> m ; 
-//         for(auto x : arr ) m[x]++ ; 
-//         priority_queue<int> q(m.begin() , m.end() ) ; 
-//         // for(auto &[f , s ] : m ){
-//         //     q.push(s)
-//         // }
+
+        
+//         Using HashMap to count the frequency of each numbers.
+// Get array of the frequency, let name it frequencies.
+// Sort frequencies in increasing order.
+// We prioritize removing the numbers with the largest frequency first, until we've eliminated at least half of the arr.
+// Return the number of numbers removed.
+//            unordered_map<int, int> cnt;
+//         for (int x : arr) ++cnt[x];
+        
+//         vector<int> frequencies;
+//         for (auto [_, freq] : cnt) frequencies.push_back(freq);
+//         sort(frequencies.begin(), frequencies.end());
+        
+//         int ans = 0, removed = 0, half = arr.size() / 2, i = frequencies.size() - 1;
+//         while (removed < half) {
+//             ans += 1;
+//             removed += frequencies[i--];
+//         }
+//         return ans;
+        
+//         Time: O(NlogN)
+//         Space: O(N)
         
         
         
+        //Using counting sort 
            unordered_map<int, int> cnt;
+        int n = arr.size() ; 
         for (int x : arr) ++cnt[x];
-        
-        vector<int> frequencies;
-        for (auto [_, freq] : cnt) frequencies.push_back(freq);
-        sort(frequencies.begin(), frequencies.end());
-        
-        int ans = 0, removed = 0, half = arr.size() / 2, i = frequencies.size() - 1;
-        while (removed < half) {
-            ans += 1;
-            removed += frequencies[i--];
+        vector<int>count(n + 1 , 0 ) ; 
+        for(auto [_ , freq] : cnt ) {
+            count[freq]++ ; 
         }
-        return ans;
+        
+        int ans = 0 , removed = 0 , half = n/2 , f = n ; 
+        while(removed < half ) {
+          
+           
+            ans += 1;
+            while (count[f] == 0) --f;
+            removed += f ;
+            --count[f];
+            
+            
+            
+         
+        }
+        return ans ; 
+        
+        
+        
+        
+        
+        
+        
+        
+//         //Using priority queue 
+//         unordered_map<int, int>mp;
+//         priority_queue<int> pq;
+// 		for(auto num : arr) mp[num]++;                  
+//         for(auto [num, freq] : mp) pq.push(freq);
+//         for(int deleted{}, ans{}; ;pq.pop()) {
+//             deleted += pq.top(), ans++;
+//             if(deleted >= size(arr) / 2) return ans;    
+//         }
+//         return -1;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
     }
 };
