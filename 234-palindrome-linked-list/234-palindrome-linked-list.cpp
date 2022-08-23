ac1545edf6c604d64246f6d18a23ed95816e1004
Sohail Rayeen <92709773+Xohail69@users.noreply.github.com>
2022-08-23 20:24:04 +0530

    Time: 330 ms (63.56%), Space: 118.1 MB (50.21%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -32,27 +32,63 @@ public:
         
         }
     bool isPalindrome(ListNode* head) {
-        vector<int> v ;
-        for(auto n = head ; n ; n = n->next ){
-            v.push_back(n->val) ; 
-        }
-        return pali(v) ;
+        // vector<int> v ;
+        // for(auto n = head ; n ; n = n->next ){
+        //     v.push_back(n->val) ; 
+        // }
+        // return pali(v) ;
         
-        ListNode *p , *q ; 
-        p = q = head ; 
-        ListNode *t  ; 
-        while( q->next ){
-            p = p->next ; 
-            q = q->next->next ; 
-            if( !q ) t = reverse(p) ; 
-            else if( !q->next ) t = reverse(p->next ) ;
+        
+        // let's find the middle element of linked list
+        ListNode *slow , *fast ; 
+        slow = fast = head ; 
+        
+        while(fast->next != NULL and fast->next->next != NULL ){
+            slow = slow->next ; 
+            fast = fast->next->next ; 
+            
         }
-        while(t ){
-            if(t->val != head->val) return 0 ; 
-            t = t->next ; 
+        
+        /*
+        [1,2,3,4,3,2,1]
+        
+        let us take this example ,after the above while loop 
+        slow is pointing to 4 and we reverse the linked list after 4 and attach it to 
+        4 and our slow becomes slow->next so that we can neglect the alone element of 
+        odd length linkedlist
+        
+        the linkedlist becomes [1 2 3 4 1 2 3]
+        and head points to first 1
+        and slow points to second 1
+        */
+        slow->next = reverse(slow->next) ; 
+        slow = slow->next ;
+          /*
+        okay ,wait ! what about even length linkedlist?
+        let's take a look on that
+        consider the linkedlist as  [1,2,3,4,4,3,2,1]
+        after the slow and fast pointer while loop our slow will be pointing to first occurance of 4 ,so we reverse the [4,3,2,1] part and attach it to first 4 
+        and we move our slow pointer to next so that our checking becomes easy as we compare the linked list like this
+        [1 2 3 4 1 2 3 4 ]and our head is pointing to first 1 and slow is pointing to second 1  
+        */
+        
+        while (slow ) {
+            if(slow->val != head->val ) return 0 ; 
+            slow = slow->next ; 
             head = head->next ; 
         }
         return 1 ; 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
     }
 };
