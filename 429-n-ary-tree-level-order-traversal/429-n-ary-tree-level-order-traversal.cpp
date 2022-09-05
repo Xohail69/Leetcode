18dc6acd85d9e561a708e1abc449b0f60c2b63f7
Sohail Rayeen <92709773+Xohail69@users.noreply.github.com>
2022-09-05 18:30:58 +0530

    Time: 51 ms (14.13%), Space: 11.7 MB (65.30%) - LeetHub

--- 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.cpp
+++ 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.cpp
@@ -19,26 +19,37 @@ public:
 */
 
 class Solution {
+    private :  vector<vector<int>> ans ; 
 public:
+    void dfs(Node* root , int level ){
+        if( level == ans.size() ) ans.emplace_back() ; 
+        ans[level].push_back( root->val ) ; 
+        for(auto &child : root->children )
+            dfs(child , level + 1 ) ; 
+    }
     vector<vector<int>> levelOrder(Node* root) {
         if(!root) return {} ;
-        vector<vector<int>> ans ; 
-        queue<Node*> q ; 
-        q.push(root) ; 
-        while(q.size() ) {
-            int n = q.size() ; 
-            ans.emplace_back() ; 
-            // vector<int> v ; 
-            while(n--){
-                auto t = q.front() ; 
-                ans.back().push_back(t->val) ; 
-                q.pop() ;
-               for(auto &child : t->children )
-                   q.push(child) ;
+        
+//         BFS
+//         vector<vector<int>> ans ; 
+//         queue<Node*> q ; 
+//         q.push(root) ; 
+//         while(q.size() ) {
+//             int n = q.size() ; 
+//             ans.emplace_back() ; 
+//             // vector<int> v ; 
+//             while(n--){
+//                 auto t = q.front() ; 
+//                 ans.back().push_back(t->val) ; 
+//                 q.pop() ;
+//                for(auto &child : t->children )
+//                    q.push(child) ;
                 
-            }
-            // ans.push_back(v) ;
-        }
+//             }
+//             // ans.push_back(v) ;
+//         }
+//         return ans; 
+        dfs(root , 0 ) ; 
         return ans; 
     }
 };
