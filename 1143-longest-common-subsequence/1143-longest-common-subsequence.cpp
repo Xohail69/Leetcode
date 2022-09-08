b77d8aafbffc972a5b2799e788475252cb00391f
Sohail Rayeen <92709773+Xohail69@users.noreply.github.com>
2022-09-08 19:38:01 +0530

    Time: 33 ms (58.31%), Space: 17.6 MB (11.91%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -14,6 +14,23 @@ public:
             return dp[x][y] = max( lcs(x , y-1  , s1 , s2 ) , lcs(x-1 , y , s1 , s2 ) ) ;
     }
     int longestCommonSubsequence(string s1, string s2) {
-        return lcs(s1.size() , s2.size()  , s1 , s2 ) ;
+        // return lcs(s1.size() , s2.size()  , s1 , s2 ) ;
+        
+        
+//         Bottom up
+        int n = s1.size() , m = s2.size() ; 
+        vector<vector<int>> dp(n+1 , vector<int>(m+1 , 0 )) ;
+//         filled with 0 cux we have to make first row & col 0 
+        for(int i = 1 ; i < n + 1 ; i++ ){
+            for(int j = 1 ; j < m + 1 ; j++ ){
+                if(s1[i-1] == s2[j-1] ){
+                    dp[i][j] = 1 + dp[i-1][j-1] ; 
+                }
+                else 
+                    dp[i][j] = max(dp[i][j-1] , dp[i-1][j] ) ; 
+            }
+        }
+        return dp[n][m] ; 
+        
     }
 };
