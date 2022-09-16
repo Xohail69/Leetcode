6738e2279db3f054c3a1b1d7c50ab5edac7ee114
Sohail Rayeen <92709773+Xohail69@users.noreply.github.com>
2022-09-16 23:32:53 +0530

    Time: 881 ms (27.24%), Space: 334.1 MB (6.54%) - LeetHub

---
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    // typedef int long ;
+ int helper(int i , int l , int r , vector<int>& nums, vector<int>& mult , vector<vector<int>> &dp) { 
+     
+        if(i == mult.size() ) {
+            return 0 ;
+        }
+        if(dp[i][l] != INT_MIN ) return dp[i][l] ; 
+        int left = nums[l]*mult[i] + helper(i + 1 , l +1 , r , nums , mult , dp ) ; 
+        int right= nums[r]*mult[i] + helper(i + 1 , l , r - 1 , nums , mult , dp ) ; 
+        return dp[i][l] = max(left , right ) ; 
+        
+    }
+    int maximumScore(vector<int>& nums, vector<int>& mult ) {
+        vector<vector<int>>dp(1e3 , vector<int>(1e3 , INT_MIN )) ; 
+       return helper(0 , 0 , nums.size() - 1 , nums , mult , dp ) ; 
+    }
+};
