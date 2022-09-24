3a37885aafa9299ea6c6a7b4c19728e76029321f
Sohail Rayeen <92709773+Xohail69@users.noreply.github.com>
2022-09-24 23:20:52 +0530

    Time: 0 ms (100.00%), Space: 12.2 MB (43.18%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.cpp
@@ -16,24 +16,73 @@ public:
         queue<TreeNode*> q ; 
         vector<vector<int>> ans ; 
         q.push(root) ; 
-        bool LtR = 1 ; 
-        while(q.size() ) {
+        // bool LtR = 1 ; 
+        // while(q.size() ) {
+        //     int n = q.size() ; 
+        //     vector<int> curr(n) ; 
+        //     for(int i = 0 ; i < n ; i++ ) {
+        //         auto t = q.front() ; 
+        //         q.pop() ;
+//         if moving right to left then place the value at back 
+        //         int ix = LtR ? i : n - i - 1 ;
+        //         curr[ix] = t->val ; 
+        //         if(t->left) 
+        //             q.push(t->left) ; 
+        //         if(t->right) 
+        //             q.push(t->right) ;
+        //     }
+        //     LtR =  !LtR  ;
+        //     ans.push_back(curr) ;
+        // }
+        // return ans; 
+        
+        
+        
+//         Using reverse function on alternate levels ; 
+        int cunt = 1; 
+        while(q.size() ){
             int n = q.size() ; 
-            vector<int> curr(n) ; 
-            for(int i = 0 ; i < n ; i++ ) {
+            vector<int> curr ; 
+            while(n--){
                 auto t = q.front() ; 
                 q.pop() ;
-                int ix = LtR ? i : n - i - 1 ;
-                curr[ix] = t->val ; 
-                if(t->left) {
+                curr.push_back(t->val) ; 
+                 if(t->left) 
                     q.push(t->left) ; 
-                }
                 if(t->right) 
                     q.push(t->right) ;
+                
             }
-            LtR =  !LtR  ;
+            if(cunt%2 == 0 ) reverse(curr.begin() , curr.end() ) ;
+            cunt++ ; 
             ans.push_back(curr) ;
         }
         return ans; 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 };
