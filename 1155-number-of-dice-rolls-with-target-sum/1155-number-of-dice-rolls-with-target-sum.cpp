c7b393142cbbebd506fe8103d92fc5cd837b3019
Sohail Rayeen <92709773+Xohail69@users.noreply.github.com>
2022-10-02 15:44:46 +0530

    Time: 242 ms (5.19%), Space: 8.6 MB (28.55%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -7,7 +7,7 @@ public:
         
         if(n == 0 and t == 0 ) { return 1 ;  }
         if(n <= 0 or t < 0 ) return 0 ; 
-        // if(n == 0 and t != 0 ) return 0 ;
+       
         if(dp[n][t] != -1 ) return dp[n][t] ; 
         int ans = 0 ;
         for(int i = 1 ; i <= k ; i++ ){
@@ -17,8 +17,23 @@ public:
     }
     
     int numRollsToTarget(int n, int k, int target) {
-        memset(dp , -1 , sizeof(dp) ) ;
-        helper(n , target , k  ) ; 
-        return dp[n][target] ;
+        // memset(dp , -1 , sizeof(dp) ) ;
+        // helper(n , target , k  ) ; 
+        // return dp[n][target] ;
+        
+        
+        vector<vector<int>> dp(n+1 , vector<int>(target+1 , 0 ) ) ; 
+        dp[0][0] = 1 ; 
+        for(int i = 1 ; i < n+1  ; i++ ){
+            for(int j = 1 ; j < target+1 ; j++ ) {
+                for(int m = 1  ; m <= k ; m++ ){
+                    dp[i][j] = (dp[i][j] + (j >= m ? dp[i-1][j-m] :0 ) )%mod ;
+                }
+                
+            }
+        }
+        return dp[n][target] ; 
+        
+        
     }
 };
