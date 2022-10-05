0d6112c27d89595ccdb2cc1943304f9fb3c8a4cb
Sohail Rayeen <92709773+Xohail69@users.noreply.github.com>
2022-10-06 01:27:19 +0530

    Time: 60 ms (17.26%), Space: 12.9 MB (56.04%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -20,6 +20,19 @@ public:
                 i-- ; j-- ; 
             }
             else{
+                
+//                 When there is a mismatch , we are ignoring character from either String x or String y.
+// dp[i-1][j] --> Ignoring the character from String x
+// dp[i][j-1] --> Ignoring the character from String y
+
+// So when dp[i][j-1] > dp[i-1][j] , this means ignoring the character from String x is giving maximum LCS. Since this ignored character will no longer involve in the comparison process, we should include this ignored character in the result.
+                 
+                
+                
+                // We have to go in the direction of maximum because this has yielded maximum LCS
+			// While moving in that direction we have to include the mismatched character 
+			// from the String which we are moving away from 
+			// Here we are moving away from String y, hence adding that character to the res
                 if(dp[i][j-1] > dp[i-1][j] ){
                     ans += y[j-1] ; 
                     j-- ; 
