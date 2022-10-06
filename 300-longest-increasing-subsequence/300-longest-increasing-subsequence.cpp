8f8b1f55bdf3da67f6a68f0f3df1d3f2f10e886b
Sohail Rayeen <92709773+Xohail69@users.noreply.github.com>
2022-10-07 02:17:00 +0530

    Time: 1423 ms (11.36%), Space: 198.8 MB (13.85%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
-     
-    
+     int lcs(vector<int> &x , vector<int> &y ) {
+        int n = x.size() , m = y.size() ; 
+         vector<vector<int>>dp( n + 1 , vector<int>(m +1 , 0 ) ) ;
+		    for(int i = 1 ; i < n + 1 ; i++ ) {
+		        for(int j = 1 ; j < m + 1 ; j++ ){
+		            if(x[i-1] == y[j-1] ) {
+		                dp[i][j] = 1 + dp[i-1][j-1] ; 
+		            }
+		            else dp[i][j] = max(dp[i-1][j] , dp[i][j-1] ) ; 
+		        }
+		    }
+		    return dp[n][m] ; 
+    }
     
     int lengthOfLIS(vector<int>& nums) {
-            int n = nums.size() ; 
-        vector<int> dp(n , 1 ) ; 
-        for(int i = 0 ; i < n ; i++ ){
-            for(int j = 0 ; j < i ; j++ ){
-                if(nums[i] > nums[j] and dp[i] < dp[j] +1 )
-                    dp[i] = dp[j] + 1 ; 
-            }
-        }
         
-        return *max_element(begin(dp)  , end(dp ) ) ; 
-        
+        set<int> s(nums.begin() , nums.end() ) ; 
+        vector<int> v (s.begin() , s.end() ) ; 
+        return lcs(nums , v )  ;
     }
 };
