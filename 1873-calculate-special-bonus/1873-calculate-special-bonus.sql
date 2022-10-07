617ddf5b9c757381f845657156782488bbb21057
Sohail Rayeen <92709773+Xohail69@users.noreply.github.com>
2022-10-07 11:40:57 +0530

    Time: 759 ms (47.67%), Space: 0B (100.00%) - LeetHub

--- 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -5,6 +5,14 @@
 # IF(condition, value_if_true, value_if_false)
 
 
-SELECT employee_id , if(employee_id%2 !=0 AND name NOT LIKE 'M%' , salary , 0 ) AS bonus
-FROM Employees order by employee_id ;
+# SELECT employee_id , if(employee_id%2 !=0 AND name NOT LIKE 'M%' , salary , 0 ) AS bonus
+# FROM Employees order by employee_id ;
 
+
+# Using Case 
+SELECT employee_id, 
+    CASE 
+    WHEN employee_id%2=1 AND name NOT LIKE 'M%' THEN salary 
+    ELSE 0 
+    END 
+AS bonus FROM Employees order by employee_id ;
