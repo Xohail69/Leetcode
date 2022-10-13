4c6598ea351e91153d74f4b71f2c27470c7dcd45
Sohail Rayeen <92709773+Xohail69@users.noreply.github.com>
2022-10-13 16:23:57 +0530

    Time: 16 ms (88.71%), Space: 13.3 MB (20.37%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -2,21 +2,51 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
         int n = matrix.size() , m = matrix[0].size() ; 
-        vector<int> row(n , 0 ) , col(m , 0) ; 
-        for(int i = 0 ; i < n ; i++ ){
-            for(int j = 0  ; j < m ; j ++ ){
+//         BF
+//         vector<int> row(n , 0 ) , col(m , 0) ; 
+//         for(int i = 0 ; i < n ; i++ ){
+//             for(int j = 0  ; j < m ; j ++ ){
+//                 if(  !matrix[i][j] ){
+//                     row[i] = 1 ;
+//                     col[j] = 1 ; 
+//                 }
+//             }
+//         }
+//           for(int i = 0 ; i < n ; i++ ){
+//             for(int j = 0  ; j < m ; j ++ ){
+//                 if( row[i] or col[j]  ){
+//                     matrix[i][j] = 0 ; 
+//                 }
+//             }
+//         }
+        
+        
+//         Optimize 
+        bool isCol = false ; 
+          for(int i = 0 ; i < n ; i++ ){
+            if(matrix[i][0] == 0 ) isCol = 1 ;
+              for(int j = 1  ; j < m ; j ++ ){
+                
                 if(  !matrix[i][j] ){
-                    row[i] = 1 ;
-                    col[j] = 1 ; 
+                    matrix[i][0] = 0 ; 
+                    matrix[0][j] = 0 ; 
                 }
             }
         }
-          for(int i = 0 ; i < n ; i++ ){
-            for(int j = 0  ; j < m ; j ++ ){
-                if( row[i] or col[j]  ){
+        
+        for(int i = n -1 ; i >=0 ; i-- ){
+            for(int j = m-1 ; j >=1 ; j-- ){
+                if(!matrix[i][0] or !matrix[0][j] ){
                     matrix[i][j] = 0 ; 
                 }
+               
             }
+             if(isCol ){
+                    matrix[i][0] = 0 ; 
+                }
         }
+        
+        
+    
     }
 };
