e726ca16b4b26964c5752854c5884d7563b73edd
Sohail Rayeen <92709773+Xohail69@users.noreply.github.com>
2022-10-13 15:19:10 +0530

    Time: 37 ms (16.18%), Space: 7.6 MB (37.95%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
@@ -9,21 +9,31 @@
 class Solution {
 public:
     void deleteNode(ListNode* node) {
-        ListNode *p  , *q ; 
-        p = node  ; 
-        q = node->next ; 
+        // ListNode *p  , *q ; 
+        // p = node  ; 
+        // q = node->next ; 
+//         Swapping till the end O(n)
         // while( q ){
         //     swap(p->val , q->val ) ; 
         //     if(!q->next ) p->next = NULL ;
         //     p = p->next ; 
         //     q = q->next ; 
         // // }
+        
+//         O(1) 
        // swap(p->val , q->val ) ; 
         // p->next  = q->next ; 
         // delete q ;
         
-        swap(*p , *q ) ; 
-        delete q ; 
+        // swap(*p , *q ) ; //Swapping the objects 
+        // delete q ; 
+        
+        
+        
+//         Best 
+        auto next = node->next ; 
+        *node = *next ; // copying the next object to the node object 
+        delete next ; 
         
         
     }
