a444ca339bcc5962e92a725dbf622680af7ca7fd
Sohail Rayeen <92709773+Xohail69@users.noreply.github.com>
2022-10-17 14:26:16 +0530

    Time: 40 ms (93.67%), Space: 19.1 MB (70.57%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -3,27 +3,33 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>&  a) {
         sort(a.begin() , a.end() ) ; 
-        // auto prev = a.begin() ; 
-        // for(auto x = a.begin() ; x != a.end() ; x++ ){
-        //     if( (*x)[0] < (*prev)[1] ){
-        //         (*prev)[1] = (*x)[1] ; 
-        //     }
-        //     prev = x ;
-        // }
-        // return a ; 
-        
+             
         vector<vector<int>> ans ; 
-        // auto prev = a.begin() ; 
+        
+        // ans.push_back(a[0]) ;
+        // for(auto x  = ++a.begin() ; x != a.end() ; x++ ){
+        //     auto prev = ans.back()  ;
+        //     auto curr = *x ; 
+        //     if( curr[0] <= prev[1] ){
+        //         ans.pop_back() ; 
+        //         ans.push_back({prev[0] , max(curr[1] , prev[1]) } )  ; 
+        //     }
+        //     else ans.push_back( curr ) ;
+        // }
+        
         ans.push_back(a[0]) ;
-        for(auto x  = ++a.begin() ; x != a.end() ; x++ ){
-            auto prev = ans.back()  ;
-            auto curr = *x ; 
-            if( curr[0] <= prev[1] ){
-                ans.pop_back() ; 
-                ans.push_back({prev[0] , max(curr[1] , prev[1]) } )  ; 
+        for(auto &curr : a ){
+            auto &prev = ans.back()  ; 
+            if(ans.empty() or curr[0] > prev[1]  ){
+                ans.push_back(curr) ; 
             }
-            else ans.push_back( curr ) ;
+            else{
+                prev[1] = max(prev[1] , curr[1] ) ; 
+            }
         }
         return ans ; 
+        
     }
 };
+
+
