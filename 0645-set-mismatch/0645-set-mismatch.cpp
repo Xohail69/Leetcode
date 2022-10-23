2244e56a7e4775594e8d298b2dcdbfc59821652c
Sohail Rayeen <92709773+Xohail69@users.noreply.github.com>
2022-10-23 23:38:06 +0530

    Time: 61 ms (71.01%), Space: 21.4 MB (86.33%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& a ) {
+        int xo = 0 ; 
+        for(auto x : a ) xo ^=x ; 
+        int n = a.size() ; 
+        for(int i =  1  ; i <=  n ; i++ ){
+            xo ^= i ; 
+        }
+        int rightmost = xo & ~(xo - 1 ) ; 
+        int x1 = 0 , x2 = 0 ; 
+        for(auto i : a ){
+            if(rightmost & i )
+                x1 ^= i ; 
+            else x2 ^= i ; 
+        }
+        for(int i = 1 ; i <= n ; i++ ){
+            
+            if(rightmost & i )
+                x1 ^= i ; 
+            else x2 ^=  i ; 
+        }
+        
+        for(auto i : a ){
+            if(i == x1 )
+                return {x1 , x2 } ; 
+        }
+        
+        return {x2 , x1  } ; 
+        
+        
+        
+    }
+};
