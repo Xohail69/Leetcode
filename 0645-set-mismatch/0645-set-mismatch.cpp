7b73086dac8b42849eaa7775c857a0cfca603e9c
Sohail Rayeen <92709773+Xohail69@users.noreply.github.com>
2022-10-23 23:52:41 +0530

    Time: 57 ms (74.22%), Space: 21.4 MB (66.56%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,32 +1,52 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& a ) {
-        int xo = 0 ; 
-        for(auto x : a ) xo ^=x ; 
-        int n = a.size() ; 
-        for(int i =  1  ; i <=  n ; i++ ){
-            xo ^= i ; 
-        }
-        int rightmost = xo & ~(xo - 1 ) ; 
-        int x1 = 0 , x2 = 0 ; 
-        for(auto i : a ){
-            if(rightmost & i )
-                x1 ^= i ; 
-            else x2 ^= i ; 
-        }
-        for(int i = 1 ; i <= n ; i++ ){
+//         int xo = 0 ; 
+//         for(auto x : a ) xo ^=x ; 
+//         int n = a.size() ; 
+//         for(int i =  1  ; i <=  n ; i++ ){
+//             xo ^= i ; 
+//         }
+//         int rightmost = xo & ~(xo - 1 ) ; 
+//         int x1 = 0 , x2 = 0 ; 
+//         for(auto i : a ){
+//             if(rightmost & i )
+//                 x1 ^= i ; 
+//             else x2 ^= i ; 
+//         }
+//         for(int i = 1 ; i <= n ; i++ ){
             
-            if(rightmost & i )
-                x1 ^= i ; 
-            else x2 ^=  i ; 
-        }
+//             if(rightmost & i )
+//                 x1 ^= i ; 
+//             else x2 ^=  i ; 
+//         }
         
+//         for(auto i : a ){
+//             if(i == x1 )
+//                 return {x1 , x2 } ; 
+//         }
+        
+        // return {x2 , x1  } ; 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        int dup = -1 , miss = -1 ; 
         for(auto i : a ){
-            if(i == x1 )
-                return {x1 , x2 } ; 
+            i = abs(i) ; 
+            if(a[i-1] < 0 )
+                dup = i  ; 
+            else a[i-1 ] *= -1  ; 
         }
-        
-        return {x2 , x1  } ; 
+        for(int  i = 0 ;i < a.size() ; i++  ) 
+            if(a[i] > 0 )
+                miss = i + 1 ; 
+        return {dup , miss } ; 
         
         
         
