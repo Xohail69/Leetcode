24c86870eac6869b5db22cadcd8ca65a7a344021
Sohail Rayeen <92709773+Xohail69@users.noreply.github.com>
2022-10-23 23:59:30 +0530

    Time: 61 ms (71.01%), Space: 21.4 MB (66.56%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -36,17 +36,67 @@ public:
         
         
         
-        int dup = -1 , miss = -1 ; 
-        for(auto i : a ){
-            i = abs(i) ; 
-            if(a[i-1] < 0 )
-                dup = i  ; 
-            else a[i-1 ] *= -1  ; 
-        }
-        for(int  i = 0 ;i < a.size() ; i++  ) 
-            if(a[i] > 0 )
-                miss = i + 1 ; 
-        return {dup , miss } ; 
+//         int dup = -1 , miss = -1 ; 
+//         for(auto i : a ){
+//             i = abs(i) ; 
+//             if(a[i-1] < 0 )
+//                 dup = i  ; 
+//             else a[i-1 ] *= -1  ; 
+//         }
+//         for(int  i = 0 ; i < a.size() ; i++  ) 
+//             if(a[i] > 0 )
+//                 miss = i + 1 ; 
+//         return {dup , miss } ; 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+         /** Assume m is the missing and d is the duplicate element
+         diff= m-d;
+         squareDiff= m^2-d^2;
+         sum=m+d= squareDiff/diff
+         =>sum    = (m+d)(m-d)/(m-d);
+         now m=(sum+diff)2;
+         and d= (sum-diff)2;
+        **/
+     long long   int diff=0;
+      long long  int sqaureDiff=0;
+        for(long long int i=0;i<a.size();i++){
+            /** 
+            The order doesnot matter. keep adding the 1 to n and simultaneously subracting  corresponding array element.
+            Use i+1 to get 1 to n  since i is the index number which is zero based.
+            **/
+            diff+=(i+1)-a[i];
+            /** squareDiff is also calculated in the same way as diff is calculated. **/
+            sqaureDiff+= (i+1)*(i+1)-a[i]*a[i];
+        } 
+       int sum=sqaureDiff/diff;
+      return {(int)(sum-diff)/2, (int)(sum+diff)/2};
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
         
         
