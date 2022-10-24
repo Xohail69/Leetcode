10a3f5da7a7df9a533eaf960049a948973dcdf02
Sohail Rayeen <92709773+Xohail69@users.noreply.github.com>
2022-10-24 23:17:07 +0530

    Time: 2854 ms (5.05%), Space: 625.1 MB (5.05%) - LeetHub

---
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool isValid(string &s ){
+        if(s.size() == 0 ) return 0 ; 
+        vector<int> h(26 , 0 ) ; 
+        for(auto x : s ){
+            h[x-'a']++ ; 
+            if(h[x-'a'] > 1 ) return 0 ; 
+        }
+        return 1 ; 
+    }
+    int ans = 0 ; 
+    void sub(int i , string s , vector<string>&a ){
+        if(i == a.size() ){
+            if(isValid(s ) ){
+                int n = s.size() ; 
+                ans = max(ans , n ) ; 
+            }
+            return ; 
+        }
+        // s += a[i] ; 
+        string t = s + a[i] ; 
+        sub(i +1 , t , a ) ; 
+        sub(i +1 , s , a ) ; 
+    }
+    int maxLength(vector<string>& a ) {
+        string t = ""; 
+        sub(0 , t , a ) ; 
+        return ans ; 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+};
