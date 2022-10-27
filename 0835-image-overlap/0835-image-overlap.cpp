49040e93123317014036be84311ced6077169331
Sohail Rayeen <92709773+Xohail69@users.noreply.github.com>
2022-10-27 23:41:15 +0530

    Time: 206 ms (43.75%), Space: 9.4 MB (93.75%) - LeetHub

---
+++ 0835-image-overlap/0835-image-overlap.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int shift(int x , int y , vector<vector<int>>& a  , vector<vector<int>>& b ){
+        int n = a.size() ; 
+        int t = 0 ; 
+        for(int i = 0 ; i < n ; i ++ ){
+            for(int j = 0 ; j < n ; j++ ){
+                if( (x + i < 0 or x + i >= n) or (y + j <0 or y + j >= n ) )
+                    continue ; 
+                if(a[i][j] + b[i+x][y+j] == 2 ) 
+                    t++ ; 
+            }
+        }
+        return t ; 
+    }
+    
+    
+    int largestOverlap(vector<vector<int>>& a , vector<vector<int>>& b ) {
+        int ans = 0 ; 
+        int n = a.size() ; 
+        for(int i = -n +1 ; i < n ; i++ ){
+            for(int j = -n+1 ; j < n ; j++ ){
+                ans = max(ans , shift(i , j , a , b ) ) ;
+                // ans = max(ans , shift(i , j , b , a ) ) ; 
+            }
+        }
+        return ans ; 
+    }
+};
