3bf20bf846d8d393b4c3505b5ade19bb83584976
Sohail Rayeen <92709773+Xohail69@users.noreply.github.com>
2022-11-07 22:21:57 +0530

    Time: 6 ms (9.01%), Space: 5.9 MB (45.85%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.cpp
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -1,14 +1,42 @@
 class Solution {
 public:
     int maximum69Number (int num) {
-        string t = to_string(num) ; 
-        for(int i= 0 ; i < t.size() ; i++ ){
-            if(t[i] == '6') {
-                t[i] = '9' ; 
-                break ; 
-            }
+        // string t = to_string(num) ; 
+        // for(int i= 0 ; i < t.size() ; i++ ){
+        //     if(t[i] == '6') {
+        //         t[i] = '9' ; 
+        //         break ; 
+        //     }
+        // }
+        // int m = stoi(t) ; 
+        // return m ; 
+        
+        
+//         M2 without conversion to string 
+//         first we'll start iterating from the zeroth position of num 
+//         & find the position of last six then add 3*position to it 
+            
+        int n = num ; 
+        int p = 1 ; //position 
+        int maxp = 0 ; //last position of six from RHS i.e ones tens thousands
+        while( n ){
+            int digit = n%10 ; 
+            if(digit == 6 )
+                maxp = p ; 
+            p *= 10 ; 
+            n /= 10 ; 
         }
-        int m = stoi(t) ; 
-        return m ; 
+        num += 3*maxp ; 
+        return num ; 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 };
