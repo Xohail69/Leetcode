3d6f6549b374d991c7a1076321cad48aae966acb
Sohail Rayeen <92709773+Xohail69@users.noreply.github.com>
2022-11-07 22:16:49 +0530

    Time: 0 ms (100.00%), Space: 6 MB (45.85%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.cpp
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -2,17 +2,13 @@ class Solution {
 public:
     int maximum69Number (int num) {
         string t = to_string(num) ; 
-        string ans ; 
         for(int i= 0 ; i < t.size() ; i++ ){
             if(t[i] == '6') {
-                ans += '9' ; 
-                ans += t.substr(i+1)  ; 
+                t[i] = '9' ; 
                 break ; 
             }
-            ans += t[i] ; 
         }
-        if(ans.size() == 0 ) return num ; 
-        int m = stoi(ans) ; 
+        int m = stoi(t) ; 
         return m ; 
     }
 };
