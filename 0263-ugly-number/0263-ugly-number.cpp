63032a1ed379c160bfee26a9db69492aead4aba2
Sohail Rayeen <92709773+Xohail69@users.noreply.github.com>
2022-11-18 22:51:04 +0530

    Time: 8 ms (11.77%), Space: 5.7 MB (98.89%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.cpp
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -7,7 +7,13 @@ public:
     while(num%2 == 0) num/=2;
     while(num%3 == 0) num/=3;
     while(num%5 == 0) num/=5;
-    return num == 1;
     
+        return num == 1;
+    
+    
+        
+    
+        
+        
     }
 };
